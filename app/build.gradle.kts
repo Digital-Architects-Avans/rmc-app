d6aa617cb1da1ed93c06abce809a9358a8ff1e3f
Brian174tech <114503613+Brian174tech@users.noreply.github.com>
2023-11-13 22:11:52 +0100

    First commit Composables
    Een eerste opzet met de volgende zaken, maar niet gelimiteerd aan:
    - 3 Composables(te refactoren uiteraard) 1. VehicleList, 2. VehicleListItem en 3. RegisterSection.
    - string.xml voor labels en beschrijvingen
    - data classes "Vehicle" en "UserData" voor vullen benodigde objecten en ter demonstratie functionaliteit composables.
    - benodigde wijzigingen op de gradle build en haar dependencies tbv imports.
    - comments met gebruikte sources.
    - Alle composables zo goed als stateless
    
    Todo:
    RMC theme toepassen.(lettertypen, kleuren, icons, etc)
    Textfields single line of scrollable-page maken
    Scaffolding, bottom/top sheets en drawers toepassen
    Input Validatie
    Refactoring logica, data en composables over meerdere files.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
 dependencies {
     // Splash API
     implementation("androidx.core:core-splashscreen:1.0.1")
-
+    implementation("androidx.activity:activity-compose:1.8.0")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
@@ -60,7 +60,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3:1.1.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
