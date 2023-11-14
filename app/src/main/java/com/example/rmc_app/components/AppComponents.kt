c9097099b1b91c6a82643207de8e31e200b67142
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-11-14 11:06:56 +0100

    Created AppComponents that will hold composables shared across screens

---
+++ app/src/main/java/com/example/rmc_app/components/AppComponents.kt
@@ -0,0 +1,8 @@
+package com.example.rmc_app.components
+
+import androidx.compose.runtime.Composable
+
+// Add Composables here that will be shared across multiple screens
+@Composable
+fun AppComponents() {
+}
