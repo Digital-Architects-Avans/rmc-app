2a519c887cad00955761e0251528dbd02244408d
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-11-14 23:58:51 +0100

    added basic shapes

--- app/src/main/java/com/example/rmc_app/ui/theme/Shape.kt
+++ app/src/main/java/com/example/rmc_app/ui/theme/Shape.kt
@@ -5,5 +5,7 @@ import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
-    small = RoundedCornerShape(50.dp),
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(8.dp),
+    large = RoundedCornerShape(0.dp),
 )
