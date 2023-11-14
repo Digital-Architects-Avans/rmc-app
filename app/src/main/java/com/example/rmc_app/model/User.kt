a3f2705f1917584cd51cc1478ba5b49529720a25
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-11-14 11:13:09 +0100

    Created User DataClass

---
+++ app/src/main/java/com/example/rmc_app/model/User.kt
@@ -0,0 +1,13 @@
+package com.example.rmc_app.model
+
+data class User(
+    val firstName: String,
+    val lastName: String,
+    val email: String,
+    val telephone: String,
+    val password: String,
+    val address: String,
+    val postalCode: String,
+    val buildingNumber: String,
+    val city: String
+)
