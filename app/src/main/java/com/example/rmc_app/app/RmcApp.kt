772b7ac4bb7384c7acb4e602ab89f487b17e78a6
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-11-14 23:59:56 +0100

    added crossfade animation when switching between composable screens

--- app/src/main/java/com/example/rmc_app/app/RmcApp.kt
+++ app/src/main/java/com/example/rmc_app/app/RmcApp.kt
@@ -1,17 +1,45 @@
 package com.example.rmc_app.app
 
+import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.rmc_app.navigation.RmcAppRouter
+import com.example.rmc_app.navigation.Screen
+import com.example.rmc_app.screens.LoginScreen
 import com.example.rmc_app.screens.RegisterScreen
+import com.example.rmc_app.screens.TermsAndConditionsScreen
 import com.example.rmc_app.ui.theme.RmcappTheme
 
 
 @Preview(showBackground = true)
 @Composable
-fun RmcApp(modifier: Modifier = Modifier) {
-    RegisterScreen()
+fun RmcApp() {
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+
+        Crossfade(targetState = RmcAppRouter.currentScreen, label = "") { currentState ->
+            when (currentState.value) {
+                is Screen.RegisterScreen -> {
+                    RegisterScreen()
+                }
+                is Screen.TermsAndConditionsScreen -> {
+                    TermsAndConditionsScreen()
+                }
+                is Screen.LoginScreen -> {
+                    LoginScreen()
+                }
+            }
+        }
+    }
 }
+
 
 @Preview
 @Composable
