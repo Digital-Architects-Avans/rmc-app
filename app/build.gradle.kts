e17a8279efaba689fb303d9e78ee87684d3ca873
Paul van der Linden <paul@jobport.nl>
2023-11-22 15:39:42 +0100

    Added lifecycle runtime & viewmodel compose dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,12 +51,15 @@ android {
 
 dependencies {
     val composeVersion = "1.5.4"
+    val lifecycleVersion = "2.6.2"
 
     // Splash API
     implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.material3:material3:1.1.2")
