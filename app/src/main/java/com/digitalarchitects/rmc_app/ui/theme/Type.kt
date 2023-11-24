571c7ac2be015aad3643a15947b603c973623b7d
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-11-24 21:49:04 +0000

    changed namespace value

--- app/src/main/java/com/digitalarchitects/rmc_app/ui/theme/Type.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.rmc_app.ui.theme
+package com.digitalarchitects.rmc_app.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
@@ -7,7 +7,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import com.example.rmc_app.R
+import com.digitalarchitects.rmc_app.R
 
 // Initialize fonts
 @Suppress("SpellCheckingInspection")
