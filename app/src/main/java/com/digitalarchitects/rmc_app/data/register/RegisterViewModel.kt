795e9f2a4b5f80771af9b9eef7c67dc1d2445e49
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-11-24 21:48:44 +0000

    changed namespace value

--- app/src/main/java/com/digitalarchitects/rmc_app/data/register/RegisterViewModel.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/data/register/RegisterViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.rmc_app.data.register
+package com.digitalarchitects.rmc_app.data.register
 
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
