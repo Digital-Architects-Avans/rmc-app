226f8911a758050618c28d4175b38aae52268201
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-12-10 16:04:48 +0000

    Moved retrofit builder from RmcApiService into Application Container

--- app/src/main/java/com/digitalarchitects/rmc_app/network/RmcApiService.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/network/RmcApiService.kt
@@ -2,27 +2,23 @@ package com.digitalarchitects.rmc_app.network
 
 import com.digitalarchitects.rmc_app.model.User
 import com.digitalarchitects.rmc_app.model.Vehicle
-import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 
-private const val BASE_URL = "https://10.0.2.2:8443"
-
-private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
-    .baseUrl(BASE_URL)
-    .build()
-
-object RmcApi {
-    val retrofitService : RmcApiService by lazy {
-        retrofit.create(RmcApiService::class.java)
-    }
-}
 
 interface RmcApiService {
+    /**
+     * Returns a [List] of [User] and this method can be called from a Coroutine.
+     * The @GET annotation indicates that the "photos" endpoint will be requested with the GET
+     * HTTP method
+     */
     @GET("user/users")
     suspend fun getUsers(): List<User>
 
+    /**
+     * Returns a [List] of [Vehicle] and this method can be called from a Coroutine.
+     * The @GET annotation indicates that the "photos" endpoint will be requested with the GET
+     * HTTP method
+     */
     @GET("vehicle/vehicles")
     suspend fun getVehicles(): List<Vehicle>
 
