3148e34b049a3b763a5992e707c8de2e11dc35a8
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-12-10 16:02:10 +0000

    Fake UserRepository to return fake test data

--- app/src/test/java/com/digitalarchitects/rmc_app/fake/FakeNetworkUserRepository.kt
+++ app/src/test/java/com/digitalarchitects/rmc_app/fake/FakeNetworkUserRepository.kt
@@ -1,12 +1,11 @@
-package com.digitalarchitects.rmc_app
+package com.digitalarchitects.rmc_app.fake
 
-import com.digitalarchitects.rmc_app.network.FakeDataSource
+import com.digitalarchitects.rmc_app.data.UserRepository
 import com.digitalarchitects.rmc_app.model.User
 import com.digitalarchitects.rmc_app.model.Vehicle
-import com.digitalarchitects.rmc_app.network.RmcApiService
 
-class FakeMarsApiService : RmcApiService {
-
+// Class inherits from userRepository interface overrides the getUsers() fun to return fake data.
+class FakeNetworkUserRepository : UserRepository {
     override suspend fun getUsers(): List<User> {
         return FakeDataSource.userList
     }
