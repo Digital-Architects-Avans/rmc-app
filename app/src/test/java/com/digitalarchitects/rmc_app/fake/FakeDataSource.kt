189d8dcfbe8dd5d39e06a474b7daa1ab22dd148d
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-12-11 23:14:21 +0000

    Added properties so the data classes match their counterparts in the rmc-api

--- app/src/test/java/com/digitalarchitects/rmc_app/fake/FakeDataSource.kt
+++ app/src/test/java/com/digitalarchitects/rmc_app/fake/FakeDataSource.kt
@@ -6,44 +6,60 @@ import com.digitalarchitects.rmc_app.model.Vehicle
 object FakeDataSource {
     val userList = listOf(
         User(
+            id = 1,
+            email = "email",
+            userType = "userType",
             firstName = "firstName",
             lastName = "lastName",
-            email = "email",
-            telephone = "telephone",
-            password = "password",
-            address = "address",
-            postalCode = "postalCode",
+            phone = "telephone",
+            street = "street",
             buildingNumber = "buildingNumber",
+            zipCode = "zipCode",
             city = "city"
         ),
         User(
+            id = 2,
+            email = "email2",
+            userType = "userType2",
             firstName = "firstName2",
             lastName = "lastName2",
-            email = "email2",
-            telephone = "telephone2",
-            password = "password2",
-            address = "address2",
-            postalCode = "postalCode2",
+            phone = "telephone2",
+            street = "street2",
             buildingNumber = "buildingNumber2",
+            zipCode = "zipCode2",
             city = "city2"
-        )
+        ),
     )
     val vehicleList = listOf(
         Vehicle(
-            plateNumber = "plateNumber",
-            status = "status",
+            id = 1,
+            userId = 1,
+            brand = "brand",
             model = "model",
-            location = "location",
-            price = "price",
-            img = 1
+            year = 1,
+            vehicleClass = "vehicleClass1",
+            engineType = "engineType1",
+            licensePlate = "licensePlate1",
+            imgLink = "imgLink1",
+            latitude = 2.0f,
+            longitude = 2.0f,
+            price = 2.0,
+            availability = true
         ),
         Vehicle(
-            plateNumber = "plateNumber2",
-            status = "status2",
+            id = 2,
+            userId = 2,
+            brand = "brand2",
             model = "model2",
-            location = "location2",
-            price = "price2",
-            img = 2
+            year = 2,
+            vehicleClass = "vehicleClass2",
+            engineType = "engineType2",
+            licensePlate = "licensePlate2",
+            imgLink = "imgLink2",
+            latitude = 2.0f,
+            longitude = 2.0f,
+            price = 2.0,
+            availability = true
         )
     )
 }
