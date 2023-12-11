558fe202abdfa00bcf80753756211861920ad117
Paul van der Linden <paul@jobport.nl>
2023-12-11 20:54:35 +0100

    Removed Scaffold from RmcApp

--- app/src/main/java/com/digitalarchitects/rmc_app/app/RmcApp.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/app/RmcApp.kt
@@ -1,19 +1,13 @@
 package com.digitalarchitects.rmc_app.app
 
 import androidx.annotation.StringRes
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.digitalarchitects.rmc_app.R
-import com.digitalarchitects.rmc_app.components.RmcAppBar
 import com.digitalarchitects.rmc_app.dummyDTO.DummyRentalDTO
 import com.digitalarchitects.rmc_app.dummyDTO.DummyUserDTO
 import com.digitalarchitects.rmc_app.dummyDTO.DummyVehicleDTO
@@ -49,89 +43,72 @@ enum class RmcScreen(@StringRes val title: Int) {
 fun RmcApp(
     navController: NavHostController = rememberNavController()
 ) {
-    val backStackEntry by navController.currentBackStackEntryAsState()
-    val currentScreen = RmcScreen.valueOf(
-        backStackEntry?.destination?.route ?: RmcScreen.Login.name
-    )
-
-    Scaffold(
-        topBar = {
-            RmcAppBar(
-                currentScreenTitle = currentScreen.title,
-                canNavigateBack = navController.previousBackStackEntry != null,
-                navigateUp = { navController.navigateUp() },
+    NavHost(
+        navController = navController,
+        startDestination = RmcScreen.Welcome.name,
+    ) {
+        composable(route = RmcScreen.Welcome.name) {
+            WelcomeScreen(
+                onRegisterButtonClicked = { navController.navigate(RmcScreen.Register.name) },
+                onLoginButtonClicked = { navController.navigate(RmcScreen.Login.name) }
             )
         }
-    ) { innerPadding ->
-        NavHost(
-            navController = navController,
-            startDestination = RmcScreen.Welcome.name,
-            modifier = Modifier.padding(innerPadding)
-        ) {
-            composable(route = RmcScreen.Welcome.name) {
-                WelcomeScreen(
-                    onRegisterButtonClicked = { navController.navigate(RmcScreen.Register.name) },
-                    onLoginButtonClicked = { navController.navigate(RmcScreen.Login.name) }
-                )
-            }
-            composable(route = RmcScreen.Register.name) {
-                RegisterScreen(
-                    onTermsAndConditionsTextClicked = { navController.navigate(RmcScreen.TermsAndConditions.name) },
-                    onLoginTextClicked = { navController.navigate(RmcScreen.Login.name) },
-                    onRegisterButtonClicked = { navController.navigate(RmcScreen.RentACar.name) }
-                )
-            }
-            composable(route = RmcScreen.TermsAndConditions.name) {
-                TermsAndConditionsScreen(navController = navController)
-            }
-            composable(route = RmcScreen.Login.name) {
-                LoginScreen(
-                    // onForgotPasswordTextClicked = { navController.navigate(RmcScreen.ForgotPassword.name) },
-                    onLoginButtonClicked = { navController.navigate(RmcScreen.RentACar.name) },
-                    onRegisterTextClicked = { navController.navigate(RmcScreen.Register.name) }
-                )
-            }
-            composable(route = RmcScreen.RentACar.name) {
-                RentACarScreen(
-                    onSearchButtonClicked = { navController.navigate(RmcScreen.Search.name) },
-                    onRentMyCarButtonClicked = { navController.navigate(RmcScreen.RentMyCar.name) },
-                    onMyRentalsButtonClicked = { navController.navigate(RmcScreen.MyRentals.name) },
-                    onMyAccountButtonClicked = { navController.navigate(RmcScreen.MyAccount.name) }
-                )
-            }
-            composable(route = RmcScreen.Search.name) {
-                SearchScreen()
-            }
-            composable(route = RmcScreen.MyRentals.name) {
-                TODO("Implement MyRentals screen")
-                // MyRentalsScreen()
-            }
-            composable(route = RmcScreen.RentMyCar.name) {
-                val listOfRentals = DummyRentalDTO()
-                val listOfVehicles = DummyVehicleDTO()
-                val user = DummyUserDTO()
-                RentOutMyCarScreen(list = listOfRentals, vehicles = listOfVehicles, user = user)
-            }
-            composable(route = RmcScreen.MyVehicles.name) {
-                val listOfVehicles = DummyVehicleDTO()
-                MyVehiclesScreen(list = listOfVehicles)
-            }
-            composable(route = RmcScreen.RegisterVehicle.name) {
-                TODO("Implement RegisterVehicle screen")
-                // RegisterVehicleScreen()
-            }
-            composable(route = RmcScreen.MyAccount.name) {
-                val user = DummyUserDTO()
-                MyAccountScreen(user = user)
-            }
-            composable(route = RmcScreen.EditAccount.name) {
-                TODO("Implement EditAccount screen")
-                // EditAccountScreen()
-            }
+        composable(route = RmcScreen.Register.name) {
+            RegisterScreen(
+                onTermsAndConditionsTextClicked = { navController.navigate(RmcScreen.TermsAndConditions.name) },
+                onLoginTextClicked = { navController.navigate(RmcScreen.Login.name) },
+                onRegisterButtonClicked = { navController.navigate(RmcScreen.RentACar.name) }
+            )
+        }
+        composable(route = RmcScreen.TermsAndConditions.name) {
+            TermsAndConditionsScreen(navController = navController)
+        }
+        composable(route = RmcScreen.Login.name) {
+            LoginScreen(
+                // onForgotPasswordTextClicked = { navController.navigate(RmcScreen.ForgotPassword.name) },
+                onLoginButtonClicked = { navController.navigate(RmcScreen.RentACar.name) },
+                onRegisterTextClicked = { navController.navigate(RmcScreen.Register.name) }
+            )
+        }
+        composable(route = RmcScreen.RentACar.name) {
+            RentACarScreen(
+                onSearchButtonClicked = { navController.navigate(RmcScreen.Search.name) },
+                onRentMyCarButtonClicked = { navController.navigate(RmcScreen.RentMyCar.name) },
+                onMyRentalsButtonClicked = { navController.navigate(RmcScreen.MyRentals.name) },
+                onMyAccountButtonClicked = { navController.navigate(RmcScreen.MyAccount.name) }
+            )
+        }
+        composable(route = RmcScreen.Search.name) {
+            SearchScreen()
+        }
+        composable(route = RmcScreen.MyRentals.name) {
+            TODO("Implement MyRentals screen")
+            // MyRentalsScreen()
+        }
+        composable(route = RmcScreen.RentMyCar.name) {
+            val listOfRentals = DummyRentalDTO()
+            val listOfVehicles = DummyVehicleDTO()
+            val user = DummyUserDTO()
+            RentOutMyCarScreen(list = listOfRentals, vehicles = listOfVehicles, user = user)
+        }
+        composable(route = RmcScreen.MyVehicles.name) {
+            val listOfVehicles = DummyVehicleDTO()
+            MyVehiclesScreen(list = listOfVehicles)
+        }
+        composable(route = RmcScreen.RegisterVehicle.name) {
+            TODO("Implement RegisterVehicle screen")
+            // RegisterVehicleScreen()
+        }
+        composable(route = RmcScreen.MyAccount.name) {
+            val user = DummyUserDTO()
+            MyAccountScreen(user = user)
+        }
+        composable(route = RmcScreen.EditAccount.name) {
+            TODO("Implement EditAccount screen")
+            // EditAccountScreen()
         }
     }
 }
-
 
 @Preview
 @Composable
