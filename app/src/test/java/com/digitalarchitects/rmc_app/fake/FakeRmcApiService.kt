cd98894b74840aa241bb0913ce3e05c4fcfa3d09
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-12-12 00:14:18 +0000

    Edited FakeRmcApiService fun to conform to getUsers fun of RmcApiService interface implementing Headers

--- app/src/test/java/com/digitalarchitects/rmc_app/fake/FakeRmcApiService.kt
+++ app/src/test/java/com/digitalarchitects/rmc_app/fake/FakeRmcApiService.kt
@@ -3,10 +3,11 @@ package com.digitalarchitects.rmc_app.fake
 import com.digitalarchitects.rmc_app.model.User
 import com.digitalarchitects.rmc_app.model.Vehicle
 import com.digitalarchitects.rmc_app.network.RmcApiService
+import retrofit2.http.Header
 
 class FakeRmcApiService : RmcApiService {
 
-    override suspend fun getUsers(): List<User> {
+    override suspend fun getUsers(@Header(value = "Authorization") token: String): List<User> {
         return FakeDataSource.userList
     }
 
