0378e566973c32665828e99789a5398df5bf5064
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-12-12 00:23:19 +0000

    Updated the Beared Token

--- app/src/main/java/com/digitalarchitects/rmc_app/data/UserRepository.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/data/UserRepository.kt
@@ -14,7 +14,7 @@ class NetworkUserRepository(
 ) : UserRepository {
     /** Fetches list of User from rmcApi*/
     // TODO(): Add token dynamically to header after a user logs in
-    override suspend fun getUsers(): List<User> = rmcApiService.getUsers("Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwOi8vMC4wLjAuMDo4MDgwL2hlbGxvIiwiaXNzIjoiaHR0cDovLzAuMC4wLjA6ODA4MC8iLCJlbWFpbCI6InN0YWZmQGVtYWlsLmNvbSIsInVzZXJUeXBlIjoiU1RBRkYiLCJ1c2VySWQiOjIzLCJleHAiOjE3MDIzMzU1MjZ9.fDAKJJcVm4SMJ-gepWYkR3wJ9dwhGKKARR6CmkQZFd4")
+    override suspend fun getUsers(): List<User> = rmcApiService.getUsers("Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwOi8vMC4wLjAuMDo4MDgwL2hlbGxvIiwiaXNzIjoiaHR0cDovLzAuMC4wLjA6ODA4MC8iLCJlbWFpbCI6InN0YWZmQGVtYWlsLmNvbSIsInVzZXJUeXBlIjoiU1RBRkYiLCJ1c2VySWQiOjIzLCJleHAiOjE3MDIzNDI1NjR9.OcdtZK2pRiNIn0QYVhctGgRCvR3f_lpsJ6lyQtOO1E0")
 
     override suspend fun getVehicles(): List<Vehicle> = rmcApiService.getVehicles()
 }
