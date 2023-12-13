3772d5ca02148fd68a4fca0aaf09af24ea1bd547
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-12-13 14:34:47 +0000

    fixed double dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,8 +88,9 @@ dependencies {
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
+
+    // Coil
     implementation("io.coil-kt:coil-compose:2.4.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
 
     // JUnit test
     testImplementation("junit:junit:4.13.2")
