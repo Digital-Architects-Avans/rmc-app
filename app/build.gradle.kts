b3f614badaff3e7dd1d97d8ab1d377cbba73b771
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-12-13 21:53:35 +0000

    changed import of kotlin serialization plugin to match other plugins

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    kotlin("plugin.serialization") version "1.9.0"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0"
 }
 
 val bundleId = "com.digitalarchitects.rmc_app"
