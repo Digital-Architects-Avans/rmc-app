a236b118204467dd2e5c17b67f1b47355fc7aa28
Paul van der Linden <vderlinden@gmail.com>
2023-12-15 23:22:04 +0100

    Added Maps API Key via Secrets Gradle plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp") version "1.9.0-1.0.13"
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 val bundleId = "com.digitalarchitects.rmc_app"
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,10 @@ plugins {
 }
 
 buildscript {
+    dependencies {
+        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
+    }
+
     extra.apply {
         set("nav_version", "2.5.3")
         set("room_version", "2.5.2")
