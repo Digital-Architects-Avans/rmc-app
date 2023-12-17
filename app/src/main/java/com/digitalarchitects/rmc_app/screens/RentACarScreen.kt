5450ab28ae71858a831a38d8fd8613cda6e12b63
Paul van der Linden <vderlinden@gmail.com>
2023-12-17 18:57:50 +0100

    Added Clustering to Map

--- app/src/main/java/com/digitalarchitects/rmc_app/screens/RentACarScreen.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/screens/RentACarScreen.kt
@@ -1,6 +1,7 @@
 package com.digitalarchitects.rmc_app.screens
 
-import android.graphics.Color
+import android.content.ContentValues.TAG
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -24,13 +25,15 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,20 +42,26 @@ import com.digitalarchitects.rmc_app.components.RmcFilledIconButton
 import com.digitalarchitects.rmc_app.components.RmcFilledTonalIconButton
 import com.digitalarchitects.rmc_app.components.RmcFloatingActionButton
 import com.digitalarchitects.rmc_app.components.RmcImgFilledIconButton
+import com.digitalarchitects.rmc_app.components.RmcMapVehicleCluster
+import com.digitalarchitects.rmc_app.components.RmcMapVehicleItem
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMapOptions
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.PinConfig
-import com.google.maps.android.compose.AdvancedMarker
+import com.google.maps.android.clustering.ClusterItem
 import com.google.maps.android.compose.CameraPositionState
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapType
 import com.google.maps.android.compose.MapUiSettings
-import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.MapsComposeExperimentalApi
+import com.google.maps.android.compose.clustering.Clustering
+import com.google.maps.android.compose.clustering.rememberClusterManager
+import com.google.maps.android.compose.clustering.rememberClusterRenderer
 import com.google.maps.android.compose.rememberCameraPositionState
+
+val startLocation = LatLng(51.583698, 4.797110)
+val userLocation = LatLng(51.469890, 5.546670)
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -62,30 +71,8 @@ fun RentACarScreen(
     onMyRentalsButtonClicked: () -> Unit,
     onMyAccountButtonClicked: () -> Unit,
 ) {
-    // Setup for GoogleMap composable
-    val startLocation = LatLng(51.583698, 4.797110)
-    val userLocation = LatLng(51.469890, 5.546670)
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(startLocation, 13f)
-    }
-
-    val mapProperties by remember {
-        mutableStateOf(
-            MapProperties(
-                mapType = MapType.NORMAL,
-                maxZoomPreference = 18f,
-                minZoomPreference = 11f
-            )
-        )
-    }
-    val mapUiSettings by remember {
-        mutableStateOf(
-            MapUiSettings(
-                compassEnabled = false,
-                mapToolbarEnabled = false,
-                zoomControlsEnabled = false
-            )
-        )
     }
 
     BottomSheetScaffold(
@@ -120,9 +107,7 @@ fun RentACarScreen(
                 verticalArrangement = Arrangement.Center
             ) {
                 Box(Modifier.fillMaxSize()) {
-                    RmcMap(
-                        properties = mapProperties,
-                        uiSettings = mapUiSettings,
+                    RmcMapClustering(
                         cameraPositionState = cameraPositionState
                     )
                 }
@@ -213,36 +198,110 @@ fun RentACarScreen(
 }
 
 @Composable
-fun RmcMap(
-    properties: MapProperties,
-    uiSettings: MapUiSettings,
+fun RmcMapClustering(
     cameraPositionState: CameraPositionState
 ) {
+    val items = remember { mutableStateListOf<RmcVehicleItem>() }
+    LaunchedEffect(Unit) {
+        items.add(
+            RmcVehicleItem(
+                LatLng(51.583698, 4.797110),
+                "Avans Hogeschool",
+                "Hogeschoollaan 1",
+                0f
+            )
+        )
+        items.add(
+            RmcVehicleItem(
+                LatLng(51.585720, 4.793230),
+                "Avans Hogeschool",
+                "Lovensdijkstraat 61",
+                0f
+            )
+        )
+        items.add(RmcVehicleItem(LatLng(51.58656, 4.77596), "Avans Hogeschool", "Bijster 7-21", 0f))
+        items.add(
+            RmcVehicleItem(
+                LatLng(51.5794365, 4.810962),
+                "Avans Hogeschool",
+                "Beukenlaan 1",
+                0f
+            )
+        )
+        items.add(RmcVehicleItem(LatLng(51.4698905, 5.5466656), "Home", "where the heart is", 0f))
+    }
+    RmcMap(items = items, cameraPositionState)
+}
+
+@OptIn(MapsComposeExperimentalApi::class)
+@Composable
+fun RmcMap(
+    items: List<RmcVehicleItem>,
+    cameraPositionState: CameraPositionState
+) {
+    val mapProperties by remember {
+        mutableStateOf(
+            MapProperties(
+                mapType = MapType.NORMAL,
+                maxZoomPreference = 18f,
+                minZoomPreference = 11f
+            )
+        )
+    }
+    val mapUiSettings by remember {
+        mutableStateOf(
+            MapUiSettings(
+                compassEnabled = false,
+                mapToolbarEnabled = false,
+                zoomControlsEnabled = true
+            )
+        )
+    }
+
     GoogleMap(
         googleMapOptionsFactory = {
             GoogleMapOptions().mapId("DEMO_MAP_ID")
         },
-        properties = properties,
-        uiSettings = uiSettings,
+        properties = mapProperties,
+        uiSettings = mapUiSettings,
         cameraPositionState = cameraPositionState
     ) {
-        // Setup pinConfig RMC
-        val glyphImage: Int = R.drawable.ic_map_marker
-        val descriptor = BitmapDescriptorFactory.fromResource(glyphImage)
-
-        val pinConfig = PinConfig.builder()
-            .setGlyph(PinConfig.Glyph(descriptor))
-            .setBackgroundColor(MaterialTheme.colorScheme.primary.toArgb())
-            .setBorderColor(Color.WHITE)
-            .build()
-
-        // Placing test marker
-        AdvancedMarker(
-            state = MarkerState(position = LatLng(51.583698, 4.797110)),
-            title = "Avans Hogeschool",
-            snippet = "Hogeschoollaan 1",
-            pinConfig = pinConfig
+        val clusterManager = rememberClusterManager<RmcVehicleItem>()
+        val renderer = rememberClusterRenderer(
+            clusterContent = { cluster ->
+                RmcMapVehicleCluster(number = cluster.size)
+            },
+            clusterItemContent = {
+                RmcMapVehicleItem()
+            },
+            clusterManager = clusterManager,
         )
+        SideEffect {
+            clusterManager ?: return@SideEffect
+            clusterManager.setOnClusterClickListener {
+                Log.d(TAG, "Cluster clicked! $it")
+                false
+            }
+            clusterManager.setOnClusterItemClickListener {
+                Log.d(TAG, "Cluster item clicked! $it")
+                false
+            }
+            clusterManager.setOnClusterItemInfoWindowClickListener {
+                Log.d(TAG, "Cluster item info window clicked! $it")
+            }
+        }
+        SideEffect {
+            if (clusterManager?.renderer != renderer) {
+                clusterManager?.renderer = renderer ?: return@SideEffect
+            }
+        }
+
+        if (clusterManager != null) {
+            Clustering(
+                items = items,
+                clusterManager = clusterManager,
+            )
+        }
     }
 }
 
@@ -255,4 +314,23 @@ fun RentACarScreenPreview() {
         onMyRentalsButtonClicked = {},
         onMyAccountButtonClicked = {}
     )
+}
+
+data class RmcVehicleItem(
+    val vehiclePosition: LatLng,
+    val vehicleTitle: String,
+    val vehicleSnippet: String,
+    val vehicleZIndex: Float,
+) : ClusterItem {
+    override fun getPosition(): LatLng =
+        vehiclePosition
+
+    override fun getTitle(): String =
+        vehicleTitle
+
+    override fun getSnippet(): String =
+        vehicleSnippet
+
+    override fun getZIndex(): Float =
+        vehicleZIndex
 }
