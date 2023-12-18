a48c29b777eb2e2136f5e981d9069c40711c9137
Paul van der Linden <vderlinden@gmail.com>
2023-12-18 23:07:50 +0100

    Added ModalBottomSheet to build ListView for vehicles

--- app/src/main/java/com/digitalarchitects/rmc_app/screens/RentACarScreen.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/screens/RentACarScreen.kt
@@ -13,6 +13,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Surface
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CarRental
@@ -23,27 +25,35 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.digitalarchitects.rmc_app.R
 import com.digitalarchitects.rmc_app.components.RmcFilledIconButton
 import com.digitalarchitects.rmc_app.components.RmcFilledTonalIconButton
 import com.digitalarchitects.rmc_app.components.RmcFloatingActionButton
 import com.digitalarchitects.rmc_app.components.RmcImgFilledIconButton
+import com.digitalarchitects.rmc_app.components.RmcListItemDivider
 import com.digitalarchitects.rmc_app.components.RmcMapVehicleCluster
 import com.digitalarchitects.rmc_app.components.RmcMapVehicleItem
+import com.digitalarchitects.rmc_app.components.RmcVehicleListItem
+import com.digitalarchitects.rmc_app.data.rentacar.RentACarViewModel
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMapOptions
 import com.google.android.gms.maps.model.CameraPosition
@@ -66,11 +76,14 @@ val userLocation = LatLng(51.469890, 5.546670)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RentACarScreen(
+    rentACarViewModel: RentACarViewModel = viewModel(),
     onSearchButtonClicked: () -> Unit,
     onRentMyCarButtonClicked: () -> Unit,
     onMyRentalsButtonClicked: () -> Unit,
     onMyAccountButtonClicked: () -> Unit,
 ) {
+    val rentACarUIState by rentACarViewModel.uiState.collectAsState()
+
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(startLocation, 13f)
     }
@@ -116,7 +129,6 @@ fun RentACarScreen(
                 modifier = Modifier.fillMaxHeight(),
                 verticalArrangement = Arrangement.SpaceBetween
             ) {
-
                 // AppBar: RMC
                 Row {
                     Row(
@@ -188,8 +200,31 @@ fun RentACarScreen(
                         RmcFloatingActionButton(
                             icon = Icons.Filled.List,
                             label = R.string.view_list,
-                            onClick = { /* TODO*/ }
+                            onClick = {
+                                rentACarViewModel.viewListButtonClicked()
+                            }
                         )
+                    }
+                }
+                // Bottom sheet #2
+                val sheetState = rememberModalBottomSheetState()
+
+                if (rentACarUIState.showListViewSheet) {
+                    ModalBottomSheet(
+                        onDismissRequest = { rentACarViewModel.viewListButtonClicked() },
+                        sheetState = sheetState,
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .verticalScroll(rememberScrollState())
+                        ) {
+                            repeat(2) {
+                                rentACarViewModel.listOfVehicles.forEach { vehicle ->
+                                    RmcVehicleListItem(vehicle)
+                                    RmcListItemDivider()
+                                }
+                            }
+                        }
                     }
                 }
             }
