91ba2d3df002bdcce805a025ce9faa7b45091d80
Paul van der Linden <vderlinden@gmail.com>
2023-12-18 23:05:52 +0100

    Added DummyVehicleDTO to ViewModel to testRmcVehicleListItem

--- app/src/main/java/com/digitalarchitects/rmc_app/data/rentacar/RentACarViewModel.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/data/rentacar/RentACarViewModel.kt
@@ -1,6 +1,7 @@
 package com.digitalarchitects.rmc_app.data.rentacar
 
 import androidx.lifecycle.ViewModel
+import com.digitalarchitects.rmc_app.dummyDTO.DummyVehicleDTO
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -10,6 +11,8 @@ class RentACarViewModel : ViewModel() {
     // Rent A Car UI state
     private val _uiState = MutableStateFlow(RentACarUIState())
     val uiState: StateFlow<RentACarUIState> = _uiState.asStateFlow()
+
+    val listOfVehicles = DummyVehicleDTO()
 
     fun onEvent(event: RentACarUIEvent) {
         TODO("Implement events")
