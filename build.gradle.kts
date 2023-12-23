a147a487dda1faad0e6a1f4a40f58ac8cb2b8c9b
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-12-23 16:46:02 +0100

    removed version from buildscript, removed duplicate plugin and

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.android.library") version "8.1.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
-}
-
-buildscript {
-    extra.apply {
-        set("nav_version", "2.5.3")
-        set("room_version", "2.5.2")
-    }
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
 }
