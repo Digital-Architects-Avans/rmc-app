25ee00ec240c18861fdf7f38e7d848a8d1eb5ec0
Paul van der Linden <vderlinden@gmail.com>
2023-12-23 17:04:20 +0100

    Added VehicleMapItem class and getVehicleMapItems member function to get map item data from ViewModel

--- app/src/main/java/com/digitalarchitects/rmc_app/data/rentacar/RentACarViewModel.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/data/rentacar/RentACarViewModel.kt
@@ -1,7 +1,11 @@
 package com.digitalarchitects.rmc_app.data.rentacar
 
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.lifecycle.ViewModel
 import com.digitalarchitects.rmc_app.dummyDTO.DummyVehicleDTO
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.clustering.ClusterItem
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -34,4 +38,46 @@ class RentACarViewModel : ViewModel() {
             }
         }
     }
+
+    fun getVehicleMapItems(): SnapshotStateList<VehicleMapItem> {
+        val items = mutableStateListOf<VehicleMapItem>()
+        listOfVehicles.forEach { vehicle ->
+            items.add(
+                VehicleMapItem(
+                    vehicleId = vehicle.id,
+                    LatLng(
+                        vehicle.latitude.toDouble(),
+                        vehicle.longitude.toDouble()
+                    ),
+                    vehicleSnippet = "${vehicle.year} - ${vehicle.brand} ${vehicle.model}",
+                    vehicleTitle = vehicle.licensePlate,
+                    vehicleZIndex = 0f
+                )
+            )
+        }
+        return items
+    }
+}
+
+data class VehicleMapItem(
+    val vehicleId: Int,
+    val vehiclePosition: LatLng,
+    val vehicleTitle: String,
+    val vehicleSnippet: String,
+    val vehicleZIndex: Float,
+) : ClusterItem {
+    fun getId(): Int =
+        vehicleId
+
+    override fun getPosition(): LatLng =
+        vehiclePosition
+
+    override fun getTitle(): String =
+        vehicleTitle
+
+    override fun getSnippet(): String =
+        vehicleSnippet
+
+    override fun getZIndex(): Float =
+        vehicleZIndex
 }
