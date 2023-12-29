5a5c872e1d2022c70281f855ba0901d1bd52c01b
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-12-29 21:37:05 +0100

    Created a UserRepositoryTest.kt for testing the UserRepository

--- app/src/test/java/com/digitalarchitects/rmc_app/UserRepositoryTest.kt
+++ app/src/test/java/com/digitalarchitects/rmc_app/UserRepositoryTest.kt
@@ -1,26 +1,26 @@
 package com.digitalarchitects.rmc_app
 
-import com.digitalarchitects.rmc_app.data.NetworkUserRepository
-import com.digitalarchitects.rmc_app.fake.FakeDataSource
-import com.digitalarchitects.rmc_app.fake.FakeRmcApiService
-import junit.framework.TestCase.assertEquals
+import com.digitalarchitects.rmc_app.data.mapper.toUserListFromRemote
+import com.digitalarchitects.rmc_app.fake.FakeRemoteDataSource
+import com.digitalarchitects.rmc_app.fake.FakeUserRepository
 import kotlinx.coroutines.test.runTest
-import org.junit.Test
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
 
 // Test the NetworkUserRepository class in a coroutine environment
-class NetworkUserRepositoryTest {
+class UserRepositoryTest {
+
+    private val userRepository = FakeUserRepository()
+
     @Test
-    fun networkUserRepository_getUsers_verifyUserList() = runTest {
+    fun userRepository_getUsers_verifyUserList() = runTest {
 
         /* By passing the fake API service, any calls to the rmcApiService property in the repository
      * result in a call to the FakeRmcApiService. By passing fake classes for dependencies, you can
      * control exactly what the dependency returns. This approach ensures that the code you are
      * testing doesn't depend on untested code or APIs that could change or have unforeseen problems
      */
-        val repository = NetworkUserRepository(
-            rmcApiService = FakeRmcApiService()
-        )
 
-        assertEquals(FakeDataSource.userList, repository.getUsers())
+        Assertions.assertEquals(FakeRemoteDataSource.userList.toUserListFromRemote(), userRepository.getAllUsers())
     }
 }
