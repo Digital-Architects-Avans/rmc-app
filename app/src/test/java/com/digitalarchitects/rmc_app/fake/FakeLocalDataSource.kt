17217b2e7ce55b4796d9bfed336d5e05fb447968
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-12-29 21:08:10 +0100

    Changed FakeDataSource to FakeLocalDataSource for unit testing purposes

--- app/src/test/java/com/digitalarchitects/rmc_app/fake/FakeLocalDataSource.kt
+++ app/src/test/java/com/digitalarchitects/rmc_app/fake/FakeLocalDataSource.kt
@@ -1,14 +1,18 @@
-package com.digitalarchitects.rmc_app.fake
 
-import com.digitalarchitects.rmc_app.model.User
-import com.digitalarchitects.rmc_app.model.Vehicle
+import com.digitalarchitects.rmc_app.model.EngineType
+import com.digitalarchitects.rmc_app.model.RentalStatus
+import com.digitalarchitects.rmc_app.model.UserType
+import com.digitalarchitects.rmc_app.room.LocalRental
+import com.digitalarchitects.rmc_app.room.LocalUser
+import com.digitalarchitects.rmc_app.room.LocalVehicle
+import kotlinx.datetime.LocalDate
 
-object FakeDataSource {
+object FakeLocalDataSource {
     val userList = listOf(
-        User(
+        LocalUser(
             id = 1,
             email = "email",
-            userType = "userType",
+            userType = UserType.STAFF,
             firstName = "firstName",
             lastName = "lastName",
             phone = "telephone",
@@ -17,10 +21,10 @@ object FakeDataSource {
             zipCode = "zipCode",
             city = "city"
         ),
-        User(
+        LocalUser(
             id = 2,
             email = "email2",
-            userType = "userType2",
+            userType = UserType.CLIENT,
             firstName = "firstName2",
             lastName = "lastName2",
             phone = "telephone2",
@@ -28,17 +32,17 @@ object FakeDataSource {
             buildingNumber = "buildingNumber2",
             zipCode = "zipCode2",
             city = "city2"
-        ),
+        )
     )
     val vehicleList = listOf(
-        Vehicle(
+        LocalVehicle(
             id = 1,
             userId = 1,
             brand = "brand",
             model = "model",
             year = 1,
             vehicleClass = "vehicleClass1",
-            engineType = "engineType1",
+            engineType = EngineType.BEV,
             licensePlate = "licensePlate1",
             imgLink = 1,
             latitude = 2.0f,
@@ -46,20 +50,46 @@ object FakeDataSource {
             price = 2.0,
             availability = true
         ),
-        Vehicle(
+        LocalVehicle(
             id = 2,
             userId = 2,
             brand = "brand2",
             model = "model2",
             year = 2,
             vehicleClass = "vehicleClass2",
-            engineType = "engineType2",
+            engineType = EngineType.BEV,
             licensePlate = "licensePlate2",
             imgLink = 2,
             latitude = 2.0f,
             longitude = 2.0f,
             price = 2.0,
             availability = true
+        )
+    )
+    val rentalList = listOf(
+        LocalRental(
+            id = 1,
+            vehicleId = 1,
+            userId = 1,
+            date = LocalDate(2021, 1, 1),
+            price = 1.0,
+            latitude = 1.0f,
+            longitude = 1.0f,
+            status = RentalStatus.PENDING,
+            distanceTravelled = 1.0,
+            score = 1
+        ),
+        LocalRental(
+            id = 2,
+            vehicleId = 2,
+            userId = 2,
+            date = LocalDate(2021, 2, 2),
+            price = 2.0,
+            latitude = 2.0f,
+            longitude = 2.0f,
+            status = RentalStatus.APPROVED,
+            distanceTravelled = 2.0,
+            score = 2
         )
     )
 }
