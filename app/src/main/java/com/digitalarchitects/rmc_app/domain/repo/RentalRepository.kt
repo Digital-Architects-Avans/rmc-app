4503055e2ddbf9b71f2aa5b9ae49577a72bc0aa1
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-12-29 21:26:57 +0100

    Created repository interface for Rental data

---
+++ app/src/main/java/com/digitalarchitects/rmc_app/domain/repo/RentalRepository.kt
@@ -0,0 +1,34 @@
+package com.digitalarchitects.rmc_app.domain.repo
+
+import com.digitalarchitects.rmc_app.model.Rental
+
+/**
+ * Repository retrieves Rental data from underlying data source (remote and local).
+ */
+interface RentalRepository {
+
+    /** Retrieves list of [Rental] from underlying data source */
+    suspend fun getAllRentals(): List<Rental>
+
+    /** Retrieves list of [Rental] from local data source */
+    suspend fun getAllRentalsFromLocalCache(): List<Rental>
+
+    /** Retrieves all [Rental] from remote data source and updates the local data source */
+    suspend fun getAllRentalsFromRemote()
+
+    /** Retrieves [Rental] by id from underlying data source */
+    suspend fun getRentalById(rentalId: Int): Rental?
+
+    /** Adds [Rental] to the underlying data source */
+    suspend fun addRental(rental: Rental)
+
+    /** Updates the [Rental] to the underlying data source */
+    suspend fun updateRental(rental: Rental)
+
+    /** Deletes the [Rental] from the underlying data source */
+    suspend fun deleteRental(rental: Rental): Result<Unit>
+
+    /** Retreives the rental date from the [Rental] */
+    suspend fun getRentalDate(rentalId: Int): String?
+
+}
