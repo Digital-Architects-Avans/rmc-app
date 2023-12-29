192ef3b2825d0eadff1dbdd59d2ba6be7d15030f
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-12-29 21:32:13 +0100

    added global variable JWT token for easy of use during development stage

--- app/src/main/java/com/digitalarchitects/rmc_app/RmcApplication.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/RmcApplication.kt
@@ -1,13 +1,13 @@
 package com.digitalarchitects.rmc_app
 
 import android.app.Application
-import com.digitalarchitects.rmc_app.data.AppContainer
-import com.digitalarchitects.rmc_app.data.DefaultAppContainer
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class RmcApplication : Application() {
-    lateinit var container: AppContainer
-    override fun onCreate() {
-        super.onCreate()
-        container = DefaultAppContainer()
+    // GLOBAL VARIABLE: STAFF JWT token for API authentication
+    // (expires on Wednesday 27 March 2024 10:38:57)
+    object GlobalVariables {
+        var token: String = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwOi8vMC4wLjAuMDo4MDgwL2hlbGxvIiwiaXNzIjoiaHR0cDovLzAuMC4wLjA6ODA4MC8iLCJlbWFpbCI6InN0YWZmQGVtYWlsLmNvbSIsInVzZXJUeXBlIjoiU1RBRkYiLCJ1c2VySWQiOjIsImV4cCI6MTcxMTUzNTkzN30.9pCat2HbE4aKqmyk5l2hXAQOSadKttxD1ymtHmARSy0"
     }
 }
