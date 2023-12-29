4e0ac3d940091ed0e7eff54759988112393b23da
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-12-29 21:21:52 +0100

    Implemented Dagger Hilt Viewmodel and Dependency Injection

--- app/src/main/java/com/digitalarchitects/rmc_app/data/myrentals/MyRentalsViewModel.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/data/myrentals/MyRentalsViewModel.kt
@@ -2,12 +2,15 @@ package com.digitalarchitects.rmc_app.data.myrentals
 
 import androidx.lifecycle.ViewModel
 import com.digitalarchitects.rmc_app.app.RmcScreen
-import com.digitalarchitects.rmc_app.room.VehicleDao
+import com.digitalarchitects.rmc_app.domain.repo.VehicleRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
 
-class MyRentalsViewModel(
-    private val vehicleDao: VehicleDao
+@HiltViewModel
+class MyRentalsViewModel @Inject constructor(
+    private val vehicleRepository: VehicleRepository
 ) : ViewModel() {
     private val _navigateToScreen = MutableStateFlow<RmcScreen?>(null)
     val navigateToScreen = _navigateToScreen.asStateFlow()
