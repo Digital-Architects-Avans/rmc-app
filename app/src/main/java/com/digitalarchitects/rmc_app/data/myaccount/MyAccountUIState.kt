ffa04838699f19419124e65a717bd989d701210e
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-12-29 21:20:29 +0100

    Changed name of Room Table name

--- app/src/main/java/com/digitalarchitects/rmc_app/data/myaccount/MyAccountUIState.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/data/myaccount/MyAccountUIState.kt
@@ -1,10 +1,10 @@
 package com.digitalarchitects.rmc_app.data.myaccount
 
 import com.digitalarchitects.rmc_app.R
-import com.digitalarchitects.rmc_app.room.UserTable
+import com.digitalarchitects.rmc_app.room.LocalUser
 
 data class MyAccountUIState(
-    val users: List<UserTable> = emptyList(),
+    val users: List<LocalUser> = emptyList(),
     val firstName: String = "",
     val lastName: String = "",
     val imageResourceId: Int = R.drawable.usericon
