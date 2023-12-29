85dcf5d78bfdd99fc64302d23ec837899d794c47
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-12-29 21:09:37 +0100

    Created a FakeRentalRepository for unit testing purposes

---
+++ app/src/test/java/com/digitalarchitects/rmc_app/fake/FakeRentalRepository.kt
@@ -0,0 +1,41 @@
+package com.digitalarchitects.rmc_app.fake
+
+import FakeLocalDataSource
+import com.digitalarchitects.rmc_app.data.mapper.toRental
+import com.digitalarchitects.rmc_app.data.mapper.toRentalListFromLocal
+import com.digitalarchitects.rmc_app.domain.repo.RentalRepository
+import com.digitalarchitects.rmc_app.model.Rental
+
+class FakeRentalRepository: RentalRepository {
+    override suspend fun getAllRentals(): List<Rental> {
+        return FakeLocalDataSource.rentalList.toRentalListFromLocal()
+    }
+
+    override suspend fun getAllRentalsFromLocalCache(): List<Rental> {
+        return FakeLocalDataSource.rentalList.toRentalListFromLocal()
+    }
+
+    override suspend fun getAllRentalsFromRemote() {
+        return
+    }
+
+    override suspend fun getRentalById(rentalId: Int): Rental? {
+        return FakeLocalDataSource.rentalList.firstOrNull { it.id == rentalId }?.toRental()
+    }
+
+    override suspend fun addRental(rental: Rental) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun updateRental(rental: Rental) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteRental(rental: Rental): Result<Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getRentalDate(rentalId: Int): String? {
+        TODO("Not yet implemented")
+    }
+}
