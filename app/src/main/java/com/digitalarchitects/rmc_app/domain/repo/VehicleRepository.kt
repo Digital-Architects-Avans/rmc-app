802f3f1975c4ab15a6d9963e2bbcbf5140afa6e1
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-12-29 21:39:39 +0100

    Created VehicleRepository interface

---
+++ app/src/main/java/com/digitalarchitects/rmc_app/domain/repo/VehicleRepository.kt
@@ -0,0 +1,34 @@
+package com.digitalarchitects.rmc_app.domain.repo
+
+import com.digitalarchitects.rmc_app.model.Vehicle
+
+/**
+ * Repository retrieves Vehicle data from underlying data source (remote and local).
+ */
+interface VehicleRepository {
+
+    /** Retrieves list of [Vehicle] from underlying data source */
+    suspend fun getAllVehicles(): List<Vehicle>
+
+    /** Retrieves list of [Vehicle] from local data source */
+    suspend fun getAllVehiclesFromLocalCache(): List<Vehicle>
+
+    /** Retrieves all [Vehicle] from remote data source and updates the local data source */
+    suspend fun getAllVehiclesFromRemote()
+
+    /** Retrieves [Vehicle] by id from underlying data source */
+    suspend fun getVehicleById(vehicleId: Int): Vehicle?
+
+    /** Adds [Vehicle] to the underlying data source */
+    suspend fun addVehicle(vehicle: Vehicle)
+
+    /** Updates the [Vehicle] to the underlying data source */
+    suspend fun updateVehicle(vehicle: Vehicle)
+
+    /** Deletes the [Vehicle] from the underlying data source */
+    suspend fun deleteVehicle(vehicle: Vehicle): Result<Unit>
+
+    /** Retrieves the model name from the [Vehicle]  */
+    suspend fun getVehicleModel(vehicleId: Int): String?
+
+}
