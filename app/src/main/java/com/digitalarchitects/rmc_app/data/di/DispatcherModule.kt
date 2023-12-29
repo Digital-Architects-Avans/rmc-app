5b770f76db044dd0246c5311e61b5a4c8f8a1a22
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-12-29 21:02:23 +0100

    Created a DispatcherModule to provide the Dispatchers.IO for DH Dependency Injection

---
+++ app/src/main/java/com/digitalarchitects/rmc_app/data/di/DispatcherModule.kt
@@ -0,0 +1,21 @@
+package com.digitalarchitects.rmc_app.data.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class IoDispatcher
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DispatcherModule {
+    @IoDispatcher
+    @Provides
+    fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+}
