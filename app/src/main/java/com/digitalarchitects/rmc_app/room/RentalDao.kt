84f9506cfc17cff892e4c358fdc8fa1f27c8c597
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-12-29 21:25:48 +0100

    Added functions to retrieve data from Room database

--- app/src/main/java/com/digitalarchitects/rmc_app/room/RentalDao.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/room/RentalDao.kt
@@ -3,29 +3,38 @@ package com.digitalarchitects.rmc_app.room
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Upsert
-import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface RentalDao {
 
-    @Query("SELECT * FROM rentaltable ORDER BY id ASC")
-    fun getRentalOrderedById(): Flow<List<RentalTable>>
+    @Query("SELECT * FROM LocalRental ORDER BY id ASC")
+    suspend fun getAllRentals(): List<LocalRental>
 
-    // TODO GET CURRENT Vehicle
-//    @Query("SELECT * FROM rentaltable LIMIT 1")
-//    fun getRental(): RentalTable
+    @Query("SELECT * FROM LocalRental WHERE id = :rentalId")
+    suspend fun getRentalById(rentalId: Int): LocalRental
 
-    @Query("SELECT date FROM rentaltable LIMIT 1")
-    fun getRentalDate(): String?
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addAllRentals(rentals: List<LocalRental>)
+
+    @Query("SELECT date FROM LocalRental WHERE id = :rentalId")
+    suspend fun getRentalDate(rentalId: Int): String?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addRental(rental: LocalRental): Long
 
     @Upsert
-    suspend fun upsertRental(rental: RentalTable)
+    suspend fun upsertRental(rental: LocalRental)
 
     @Insert
-    suspend fun insertRental(rental: RentalTable)
+    suspend fun insertRental(rental: LocalRental)
 
     @Delete
-    suspend fun deleteRental(rental: RentalTable)
+    suspend fun deleteRental(rental: LocalRental)
+
+// TODO GET CURRENT Vehicle
+//    @Query("SELECT * FROM rentaltable LIMIT 1")
+//    fun getRental(): RentalTable
 }
