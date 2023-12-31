5d25a521a1f37bf43480e0668399df256ad7feb2
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-12-31 20:23:14 +0100

    converted into HiltViewModel

--- app/src/main/java/com/digitalarchitects/rmc_app/data/myrentals/MyRentalsViewModel.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/data/myrentals/MyRentalsViewModel.kt
@@ -2,13 +2,16 @@ package com.digitalarchitects.rmc_app.data.myrentals
 
 import androidx.lifecycle.ViewModel
 import com.digitalarchitects.rmc_app.app.RmcScreen
-import com.digitalarchitects.rmc_app.room.VehicleDao
+import com.digitalarchitects.rmc_app.domain.repo.VehicleRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
 
-class MyRentalsViewModel(
-    private val vehicleDao: VehicleDao
+@HiltViewModel
+class MyRentalsViewModel @Inject constructor(
+    private val vehicleRepository: VehicleRepository
 ) : ViewModel() {
     private val _navigateToScreen = MutableStateFlow<RmcScreen?>(null)
     val navigateToScreen = _navigateToScreen.asStateFlow()
