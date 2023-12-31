6e9b1ce50a93053f9bb907e42ef80556301aee1c
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-12-31 19:30:07 +0100

    added uiState, onEvent handlers, authenticate function

--- app/src/main/java/com/digitalarchitects/rmc_app/data/welcome/WelcomeViewModel.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/data/welcome/WelcomeViewModel.kt
@@ -1,24 +1,75 @@
 package com.digitalarchitects.rmc_app.data.welcome
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.digitalarchitects.rmc_app.app.RmcScreen
+import com.digitalarchitects.rmc_app.data.auth.AuthResult
+import com.digitalarchitects.rmc_app.data.register.RegisterUIState
+import com.digitalarchitects.rmc_app.domain.repo.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class WelcomeViewModel @Inject constructor() : ViewModel() {
+class WelcomeViewModel @Inject constructor(
+    private val userRepository: UserRepository
+) : ViewModel() {
 
     private val _navigateToScreen = MutableStateFlow<RmcScreen?>(null)
     val navigateToScreen = _navigateToScreen.asStateFlow()
 
-    // TODO MAKE onEvent Logic
-    fun onRegisterButtonClicked() {
-        _navigateToScreen.value = RmcScreen.Register
+    private val _uiState = MutableStateFlow(RegisterUIState())
+    val uiState: StateFlow<RegisterUIState> = _uiState.asStateFlow()
+
+    private val resultChannel = Channel<AuthResult<Unit>>()
+    val authResult = resultChannel.receiveAsFlow()
+
+    // Authenticate user on init by checking validity of token in shared preferences
+    init {
+        authenticate()
     }
 
-    fun onLogInButtonClicked() {
-        _navigateToScreen.value = RmcScreen.Login
+    fun onEvent(event: WelcomeUIEvent) {
+        when (event) {
+
+            is WelcomeUIEvent.RegisterButtonClicked -> {
+                _navigateToScreen.value = RmcScreen.Register
+            }
+
+            is WelcomeUIEvent.LoginButtonClicked -> {
+                _navigateToScreen.value = RmcScreen.Login
+            }
+
+            is WelcomeUIEvent.Authorized -> {
+                _navigateToScreen.value = RmcScreen.RmcTestScreen
+            }
+
+            is WelcomeUIEvent.Unauthorized -> {
+                _navigateToScreen.value = RmcScreen.Welcome
+            }
+
+            is WelcomeUIEvent.NoConnectionError -> {
+                _navigateToScreen.value = RmcScreen.Welcome
+            }
+
+            is WelcomeUIEvent.UnknownError -> {
+                _navigateToScreen.value = RmcScreen.Welcome
+            }
+        }
+    }
+
+    // Authenticate user using the JWT token stored in shared preferences
+    private fun authenticate() {
+        viewModelScope.launch {
+            _uiState.value = _uiState.value.copy(isLoading = true)
+            val result = userRepository.authenticate()
+            resultChannel.send(result)
+            _uiState.value = _uiState.value.copy(isLoading = false)
+        }
     }
 }
