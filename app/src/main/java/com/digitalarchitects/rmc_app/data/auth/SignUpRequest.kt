e1530e0ed8198e628482fd1354f4f0ae2c8cf6da
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-12-31 19:34:34 +0100

    created SignUpRequest data class

---
+++ app/src/main/java/com/digitalarchitects/rmc_app/data/auth/SignUpRequest.kt
@@ -0,0 +1,16 @@
+package com.digitalarchitects.rmc_app.data.auth
+
+import com.digitalarchitects.rmc_app.model.UserType
+
+data class SignUpRequest(
+    val email: String,
+    val password: String,
+    val userType: UserType,
+    val firstName: String,
+    val lastName: String,
+    val phone: String,
+    val street: String,
+    val buildingNumber: String,
+    val zipCode: String,
+    val city: String,
+)
