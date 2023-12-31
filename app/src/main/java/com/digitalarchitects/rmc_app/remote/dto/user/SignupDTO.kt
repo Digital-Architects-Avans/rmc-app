a42a37e81b93c04608acb838d4dc09fcb955903b
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-12-31 19:34:54 +0100

    adjusted SignupDTO to fit RegisterScreen data

--- app/src/main/java/com/digitalarchitects/rmc_app/remote/dto/user/SignupDTO.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/remote/dto/user/SignupDTO.kt
@@ -1,11 +1,16 @@
 package com.digitalarchitects.rmc_app.remote.dto.user
 
-import com.digitalarchitects.rmc_app.model.UserType
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class SignupDTO (
+data class SignupDTO(
     val email: String,
-    val userType: UserType,
-    val password: String
+    val password: String,
+    val firstName: String,
+    val lastName: String,
+    val phone: String,
+    val street: String,
+    val buildingNumber: String,
+    val zipCode: String,
+    val city: String,
 )
