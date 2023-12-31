d0e5b9b497243d5a2c22acd4163512fb1d1b2aa2
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-12-31 19:32:13 +0100

    created ui events for LoginScreen

--- app/src/main/java/com/digitalarchitects/rmc_app/data/login/LoginUIEvent.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/data/login/LoginUIEvent.kt
@@ -1,13 +1,15 @@
 package com.digitalarchitects.rmc_app.data.login
 
-import com.digitalarchitects.rmc_app.data.editmyaccount.EditMyAccountUIEvent
-
 sealed class LoginUIEvent {
     data class EmailChanged(val email: String) : LoginUIEvent()
     data class PasswordChanged(val password: String) : LoginUIEvent()
     object LoginButtonClicked : LoginUIEvent()
     object RegisterButtonClicked: LoginUIEvent()
     object NavigateUpButtonClicked: LoginUIEvent()
+    object Authorized : LoginUIEvent()
+    object Unauthorized : LoginUIEvent()
+    object UnknownError : LoginUIEvent()
+    object NoConnectionError : LoginUIEvent()
 
 
 }
