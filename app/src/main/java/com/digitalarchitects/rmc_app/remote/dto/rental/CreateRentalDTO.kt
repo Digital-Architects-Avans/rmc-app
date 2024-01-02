2de041f649aa16ccebe38f1936d22a5a04c97e8c
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-01-02 13:17:49 +0100

    added correct values for CreateRentalDTO

--- app/src/main/java/com/digitalarchitects/rmc_app/remote/dto/rental/CreateRentalDTO.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/remote/dto/rental/CreateRentalDTO.kt
@@ -1,9 +1,18 @@
 package com.digitalarchitects.rmc_app.remote.dto.rental
 
+import com.digitalarchitects.rmc_app.model.RentalStatus
 import kotlinx.datetime.LocalDate
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class CreateRentalDTO(
-    val date: LocalDate
+    val vehicleId: Int,
+    val userId: Int,
+    val date: LocalDate,
+    val price: Double,
+    val latitude: Float,
+    val longitude: Float,
+    val status: RentalStatus,
+    val distanceTravelled: Double,
+    val score: Int
 )
