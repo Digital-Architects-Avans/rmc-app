22c0f22af880f48da3d814b6856d7572b665f45f
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-01-04 23:03:37 +0100

    implemented logic to get all and current user

--- app/src/main/java/com/digitalarchitects/rmc_app/presentation/screens/myaccount/MyAccountViewModel.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/presentation/screens/myaccount/MyAccountViewModel.kt
@@ -1,13 +1,11 @@
 package com.digitalarchitects.rmc_app.presentation.screens.myaccount
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.digitalarchitects.rmc_app.data.di.IoDispatcher
-import com.digitalarchitects.rmc_app.data.local.LocalUser
 import com.digitalarchitects.rmc_app.domain.model.User
-import com.digitalarchitects.rmc_app.domain.model.UserType
 import com.digitalarchitects.rmc_app.domain.repo.UserRepository
-import com.digitalarchitects.rmc_app.presentation.RmcScreen
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
@@ -24,18 +22,26 @@ class MyAccountViewModel @Inject constructor(
     private val userRepository: UserRepository,
     @IoDispatcher private val dispatcher: CoroutineDispatcher
 ) : ViewModel() {
-    private val _navigateToScreen = MutableStateFlow<RmcScreen?>(null)
-    val navigateToScreen = _navigateToScreen.asStateFlow()
+
     private val _state = MutableStateFlow(MyAccountUIState())
     private val _uiState = _state
     val uiState: StateFlow<MyAccountUIState> = _uiState.asStateFlow()
 
+    init {
+        getUsers()
+    }
 
-    fun getUsers() {
+    private fun getUsers() {
         viewModelScope.launch(dispatcher) {
 
             val result: Result<List<User>> = runCatching {
                 userRepository.getAllUsers()
+            }
+
+            result.onSuccess { listResult ->
+                uiState.value.users = listResult
+            }.onFailure { e ->
+                e.printStackTrace()
             }
         }
     }
@@ -45,68 +51,40 @@ class MyAccountViewModel @Inject constructor(
             is MyAccountUIEvent.ShowUser -> {
                 try {
                     runBlocking {
-                        val firstName = withContext(Dispatchers.IO) {
-                            userRepository.getFirstName("659579bb24eca74f47b8d194")
+                        withContext(Dispatchers.IO) {
+                            val userId = userRepository.getCurrentUserIdFromDataStore()
+                            val currentUser = uiState.value.users.find { it.userId == userId }
+                            _state.value.currentUser = currentUser
                         }
-                        _state.value = _state.value.copy(firstName = "$firstName")
                     }
                 } catch (e: Exception) {
-                    _state.value = _state.value.copy(firstName = "Error")
+                    Log.d("MyAccountViewModel", "error: $e")
                 }
+            }
+
+            is MyAccountUIEvent.UpsertUser -> {
+                TODO()
             }
 
             is MyAccountUIEvent.InsertUser -> {
                 try {
-                    val localUser = LocalUser(
-                        userId = "1",
-                        email = "john.doe@example.com",
-                        password = "password",
-                        salt = "salt",
-                        userType = UserType.CLIENT,
-                        firstName = "John",
-                        lastName = "Doe",
-                        phone = "+1234567890",
-                        street = "Main St",
-                        buildingNumber = "123",
-                        zipCode = "12345",
-                        city = "Cityville",
-                        imageResourceId = null
-                    )
                     runBlocking {
                         val firstName = withContext(Dispatchers.IO) {
                             // TODO("Implement UI logic to add user database")
                             // userRepository.addUser(localUser.toUser())
                         }
-                        _state.value = _state.value.copy(firstName = "$firstName")
+                        //_state.value = _state.value.copy(firstName = "$firstName")
                     }
                 } catch (e: Exception) {
 //                    _state.value = _state.value.copy(firstName = "Error")
                 }
             }
 
-            is MyAccountUIEvent.onEditMyAccountButtonClicked -> {
-                _navigateToScreen.value = RmcScreen.EditMyAccount
-            }
 
-            is MyAccountUIEvent.onLogoutButtonClicked -> {
+            is MyAccountUIEvent.OnLogoutButtonClicked -> {
                 // TODO: LOG OUT FUNCTION
-                _navigateToScreen.value = RmcScreen.Welcome
 
             }
-
-            is MyAccountUIEvent.onMyRentalsButtonClicked -> {
-                _navigateToScreen.value = RmcScreen.MyRentals
-            }
-
-            is MyAccountUIEvent.onMyVehiclesButtonClicked -> {
-                _navigateToScreen.value = RmcScreen.MyVehicles
-            }
-
-            is MyAccountUIEvent.onRentOutMyCarButtonClicked -> {
-                _navigateToScreen.value = RmcScreen.RentOutMyCar
-            }
-
-            is MyAccountUIEvent.UpsertUser -> TODO()
         }
     }
 }
