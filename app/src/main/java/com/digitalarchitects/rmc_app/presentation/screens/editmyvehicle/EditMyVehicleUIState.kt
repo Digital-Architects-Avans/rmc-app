6f7192c64ccd515b3579a18c3f8dcbb6902b67f2
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-01-07 00:14:06 +0100

    changed default values

--- app/src/main/java/com/digitalarchitects/rmc_app/presentation/screens/editmyvehicle/EditMyVehicleUIState.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/presentation/screens/editmyvehicle/EditMyVehicleUIState.kt
@@ -3,8 +3,8 @@ package com.digitalarchitects.rmc_app.presentation.screens.editmyvehicle
 import com.digitalarchitects.rmc_app.domain.model.EngineType
 
 data class EditMyVehicleUIState (
-    val id: String = "1",
-    val userId: String = "1",
+    val vehicleId: String = "",
+    val userId: String = "",
     val brand: String = "",
     val model: String = "",
     val year: Int = 1990,
@@ -12,8 +12,8 @@ data class EditMyVehicleUIState (
     val engineType: EngineType = EngineType.ICE,
     val licensePlate: String = "",
     val imgLink: Int = 1,
-    val latitude: Float =51.72F,
-    val longitude: Float = 5.42F,
-    val price: Double = 99.00,
+    val latitude: Float = 0.00F,
+    val longitude: Float = 0.00F,
+    val price: Double = 0.00,
     val availability: Boolean = false
 )
