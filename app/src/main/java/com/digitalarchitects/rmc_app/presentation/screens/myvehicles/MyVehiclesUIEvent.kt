ab3d07553d5368e453bd7f0e2eff42d485a59108
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-01-06 17:51:51 +0100

    removed old event and added FetchVehicles event

--- app/src/main/java/com/digitalarchitects/rmc_app/presentation/screens/myvehicles/MyVehiclesUIEvent.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/presentation/screens/myvehicles/MyVehiclesUIEvent.kt
@@ -2,8 +2,6 @@ package com.digitalarchitects.rmc_app.presentation.screens.myvehicles
 
 sealed interface MyVehiclesUIEvent {
 
-    object NewVehicleButtonClicked: MyVehiclesUIEvent
-
-    object ShowVehicles: MyVehiclesUIEvent
+    object FetchVehicles: MyVehiclesUIEvent
 
 }
