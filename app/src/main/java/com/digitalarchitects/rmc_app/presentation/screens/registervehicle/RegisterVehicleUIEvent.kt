717bfe7c341eb4d7cf72ad265d587d20b647d672
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-01-06 17:53:27 +0100

    created RegisterVehicleUIEvent

--- app/src/main/java/com/digitalarchitects/rmc_app/presentation/screens/registervehicle/RegisterVehicleUIEvent.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/presentation/screens/registervehicle/RegisterVehicleUIEvent.kt
@@ -1,27 +1,21 @@
 package com.digitalarchitects.rmc_app.presentation.screens.registervehicle
 
-import com.digitalarchitects.rmc_app.domain.model.EngineType
-
 sealed interface RegisterVehicleUIEvent {
+    object FetchUserId : RegisterVehicleUIEvent
     object EngineTypeICEButtonClicked : RegisterVehicleUIEvent
     object EngineTypeBEVButtonClicked : RegisterVehicleUIEvent
     object EngineTypeFCEVButtonClicked : RegisterVehicleUIEvent
     object AvailabilityToggleButtonClicked : RegisterVehicleUIEvent
-    object NavigateUpButtonClicked : RegisterVehicleUIEvent
     object ConfirmRegisterVehicleButtonClicked : RegisterVehicleUIEvent
-
     //    object CancelRegisterVehicleButtonClicked:RegisterVehicleUIEvent
-    data class SetId(val id: Int) : RegisterVehicleUIEvent
-    data class SetUserId(val userId: Int) : RegisterVehicleUIEvent
     data class SetBrand(val brand: String) : RegisterVehicleUIEvent
     data class SetModel(val model: String) : RegisterVehicleUIEvent
     data class SetYear(val year: Int) : RegisterVehicleUIEvent
     data class SetVehicleClass(val vehicleClass: String) : RegisterVehicleUIEvent
-    data class SetEngineType(val engineType: EngineType) : RegisterVehicleUIEvent
     data class SetLicensePlate(val licensePlate: String) : RegisterVehicleUIEvent
-    data class SetImgLink(val imgLink: String) : RegisterVehicleUIEvent
+    data class SetImgLink(val imgLink: Int) : RegisterVehicleUIEvent
     data class SetLatitude(val latitude: Float) : RegisterVehicleUIEvent
     data class SetLongitude(val longitude: Float) : RegisterVehicleUIEvent
     data class SetPrice(val price: Double) : RegisterVehicleUIEvent
-    data class SetAvailability(val availability: Boolean) : RegisterVehicleUIEvent
+    object ResetVehicleUpdated: RegisterVehicleUIEvent
 }
