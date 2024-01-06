bb0f129f9afff0b62f62253e152381727bd14806
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-01-06 17:55:19 +0100

    fixed RegisterVehicleUIState

--- app/src/main/java/com/digitalarchitects/rmc_app/presentation/screens/registervehicle/RegisterVehicleUIState.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/presentation/screens/registervehicle/RegisterVehicleUIState.kt
@@ -3,17 +3,16 @@ package com.digitalarchitects.rmc_app.presentation.screens.registervehicle
 import com.digitalarchitects.rmc_app.domain.model.EngineType
 
 data class RegisterVehicleUIState(
-    val id: Int = 0,
-    val userId: Int = 0,
+    val userId: String = "",
     val brand: String = "",
     val model: String = "",
-    val year: String = "",
+    val year: Int? = null,
     val vehicleClass: String = "",
     val engineType: EngineType = EngineType.ICE,
     val licensePlate: String = "",
-    val imgLink: String = "",
-    val latitude: String = "",
-    val longitude: String = "",
-    val price: String = "",
+    val imgLink: Int = 0,
+    val latitude: Float = 0.0F,
+    val longitude: Float = 0.0F,
+    val price: Double = 0.00,
     val availability: Boolean = false
 )
