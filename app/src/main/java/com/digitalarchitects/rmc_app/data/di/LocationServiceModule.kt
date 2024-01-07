2935c5d414c585f4a32e3e85333ae87312c2680a
Paul van der Linden <vderlinden@gmail.com>
2024-01-07 16:23:11 +0100

    Added LocationServiceModule to data/di

---
+++ app/src/main/java/com/digitalarchitects/rmc_app/data/di/LocationServiceModule.kt
@@ -0,0 +1,26 @@
+package com.digitalarchitects.rmc_app.data.di
+
+import android.content.Context
+import com.digitalarchitects.rmc_app.data.remote.ILocationService
+import com.digitalarchitects.rmc_app.data.remote.LocationService
+import com.google.android.gms.location.LocationServices
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object LocationModule {
+
+    @Singleton
+    @Provides
+    fun provideLocationClient(
+        @ApplicationContext context: Context
+    ): ILocationService = LocationService(
+        context,
+        LocationServices.getFusedLocationProviderClient(context)
+    )
+}
