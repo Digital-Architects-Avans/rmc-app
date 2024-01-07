d5ac016f5139cbb3f2547be51029664b0276cd16
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-01-07 15:26:43 +0100

    fixed implementation of UserRepository to implement members

---
+++ app/src/androidTest/java/com/digitalarchitects/rmc_app/fake/FakeUserRepository.kt
@@ -0,0 +1,64 @@
+package com.digitalarchitects.rmc_app.fake
+
+import com.digitalarchitects.rmc_app.data.auth.AuthResult
+import com.digitalarchitects.rmc_app.data.auth.SignUpRequest
+import com.digitalarchitects.rmc_app.data.remote.dto.user.SignupDTO
+import com.digitalarchitects.rmc_app.data.remote.dto.user.UpdateUserDTO
+import com.digitalarchitects.rmc_app.domain.model.User
+import com.digitalarchitects.rmc_app.domain.repo.UserRepository
+
+class FakeUserRepository : UserRepository {
+    private val signInResults: MutableMap<Pair<String, String>, AuthResult<Unit>> = mutableMapOf()
+
+    fun setSignInResult(email: String, password: String, result: AuthResult<Unit>) {
+        signInResults[email to password] = result
+    }
+
+    override suspend fun signUp(request: SignUpRequest): AuthResult<Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun signIn(username: String, password: String): AuthResult<Unit> {
+        return signInResults[username to password] ?: AuthResult.UnknownError()
+    }
+
+    override suspend fun authenticate(): AuthResult<Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getAllUsers(): List<User> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getAllUsersFromLocalCache(): List<User> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getAllUsersFromRemote() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getUserById(userId: String): User {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun addUser(user: User, signupDTO: SignupDTO) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun updateUser(userId: String, updatedUser: UpdateUserDTO) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteUser(user: User): Result<Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getFirstName(userId: String): String? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getCurrentUserIdFromDataStore(): String? {
+        TODO("Not yet implemented")
+    }
+}
