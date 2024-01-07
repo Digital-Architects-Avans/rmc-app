a027984b4ea98d92a1d6fe9fb35ebf4ef22849be
Paul van der Linden <vderlinden@gmail.com>
2024-01-07 16:24:28 +0100

    Added LocationService to data/remote

---
+++ app/src/main/java/com/digitalarchitects/rmc_app/data/remote/LocationService.kt
@@ -0,0 +1,77 @@
+package com.digitalarchitects.rmc_app.data.remote
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Looper
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.Priority
+import com.google.android.gms.maps.model.LatLng
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import javax.inject.Inject
+
+interface ILocationService {
+    fun requestLocationUpdates(): Flow<LatLng?>
+    fun requestCurrentLocation(): Flow<LatLng?>
+}
+
+class LocationService @Inject constructor(
+    private val context: Context,
+    private val locationClient: FusedLocationProviderClient
+) : ILocationService {
+    @SuppressLint("MissingPermission")
+    @RequiresApi(Build.VERSION_CODES.S)
+    override fun requestLocationUpdates(): Flow<LatLng?> = callbackFlow {
+
+        if (!context.hasLocationPermission()) {
+            trySend(null)
+            return@callbackFlow
+        }
+
+        val request = LocationRequest.Builder(10000L)
+            .setIntervalMillis(10000L)
+            .setPriority(Priority.PRIORITY_HIGH_ACCURACY)
+            .build()
+
+        val locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                locationResult.locations.lastOrNull()?.let {
+                    trySend(LatLng(it.latitude, it.longitude))
+                }
+            }
+        }
+
+        locationClient.requestLocationUpdates(
+            request,
+            locationCallback,
+            Looper.getMainLooper()
+        )
+
+        awaitClose {
+            locationClient.removeLocationUpdates(locationCallback)
+        }
+    }
+
+    override fun requestCurrentLocation(): Flow<LatLng?> {
+        TODO("Not yet implemented")
+    }
+}
+
+fun Context.hasLocationPermission(): Boolean {
+    return ContextCompat.checkSelfPermission(
+        this,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+        this,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+}
