a66e9774b053651b0d71579850ba07577156d35f
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-01-09 12:27:37 +0100

    added fatching of Remote Data on Authorization

--- app/src/main/java/com/digitalarchitects/rmc_app/presentation/screens/login/LoginViewModel.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/presentation/screens/login/LoginViewModel.kt
@@ -3,8 +3,12 @@ package com.digitalarchitects.rmc_app.presentation.screens.login
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.digitalarchitects.rmc_app.data.auth.AuthResult
+import com.digitalarchitects.rmc_app.data.di.IoDispatcher
+import com.digitalarchitects.rmc_app.domain.repo.RentalRepository
 import com.digitalarchitects.rmc_app.domain.repo.UserRepository
+import com.digitalarchitects.rmc_app.domain.repo.VehicleRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -15,8 +19,11 @@ import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
-    private val userRepository: UserRepository
-): ViewModel() {
+    private val userRepository: UserRepository,
+    private val vehicleRepository: VehicleRepository,
+    private val rentalRepository: RentalRepository,
+    @IoDispatcher private val dispatcher: CoroutineDispatcher
+) : ViewModel() {
 
     private val _uiState = MutableStateFlow(LoginUIState())
     val uiState: StateFlow<LoginUIState> = _uiState.asStateFlow()
@@ -54,6 +61,15 @@ class LoginViewModel @Inject constructor(
             resultChannel.send(result)
             _uiState.value = _uiState.value.copy(isLoading = false)
         }
+    }
+
+    fun getDataFromRemoteSource() {
+        viewModelScope.launch(dispatcher) {
+            userRepository.getAllUsersFromRemote()
+            vehicleRepository.getAllVehiclesFromRemote()
+            rentalRepository.getAllRentalsFromRemote()
+        }
+
     }
 
 }
