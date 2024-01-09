cc1607cd007c9e9dbc30f96e48be280f9cf9cca5
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-01-09 17:20:06 +0100

    created MyRentalsUIState

--- app/src/main/java/com/digitalarchitects/rmc_app/presentation/screens/myrentals/MyRentalsUIState.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/presentation/screens/myrentals/MyRentalsUIState.kt
@@ -1,12 +1,20 @@
 package com.digitalarchitects.rmc_app.presentation.screens.myrentals
 
 import com.digitalarchitects.rmc_app.R
+import com.digitalarchitects.rmc_app.domain.model.Rental
+import com.digitalarchitects.rmc_app.domain.model.User
+import com.digitalarchitects.rmc_app.domain.model.Vehicle
 
 data class MyRentalsUIState (
-    var selectedTab: MyRentalTab = MyRentalTab.OPEN
+    var selectedTab: MyRentalTab = MyRentalTab.OPEN,
+    val isLoading: Boolean = false,
+    var myRentalsList: List<Triple<Rental, Vehicle, User>> = emptyList(),
+    var myOpenRentalsList: List<Triple<Rental, Vehicle, User>> = emptyList(),
+    var myHistoryRentalList: List<Triple<Rental, Vehicle, User>> = emptyList(),
+    val selectedRentalItem: Triple<Rental, Vehicle, User>? = null
 )
 
-enum class MyRentalTab(val tabNameResourceId: Int){
+enum class MyRentalTab(val tabNameResourceId: Int) {
     OPEN(R.string.open),
     HISTORY(R.string.history)
 }
