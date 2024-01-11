6c4c84b935798b54ff3634a22f9962749c913407
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-01-11 13:26:49 +0100

    Moved UIActions to UIEvents

--- app/src/main/java/com/digitalarchitects/rmc_app/domain/model/AddressUIAction.kt
+++
@@ -1,9 +0,0 @@
-package com.digitalarchitects.rmc_app.domain.model
-
-sealed class AddressUIAction {
-
-    object OnAddressAutoCompleteDone : AddressUIAction()
-    object OnAddressAutoCompleteClear : AddressUIAction()
-    data class OnAddressChange(val address: String) : AddressUIAction()
-    data class OnAddressSelected(val selectedPlaceItem: PlaceItem) : AddressUIAction()
-}
