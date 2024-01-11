95be9e3bbd59b3679f8a409b1aad41b1b276af4b
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-01-11 13:06:37 +0100

    implemented Location Google Place API autosuggestions

--- app/src/main/java/com/digitalarchitects/rmc_app/presentation/screens/registervehicle/RegisterVehicleUIEvent.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/presentation/screens/registervehicle/RegisterVehicleUIEvent.kt
@@ -1,5 +1,7 @@
 package com.digitalarchitects.rmc_app.presentation.screens.registervehicle
 
+import com.digitalarchitects.rmc_app.domain.model.PlaceItem
+
 sealed interface RegisterVehicleUIEvent {
     object FetchUserId : RegisterVehicleUIEvent
     object EngineTypeICEButtonClicked : RegisterVehicleUIEvent
@@ -7,15 +9,17 @@ sealed interface RegisterVehicleUIEvent {
     object EngineTypeFCEVButtonClicked : RegisterVehicleUIEvent
     object AvailabilityToggleButtonClicked : RegisterVehicleUIEvent
     object ConfirmRegisterVehicleButtonClicked : RegisterVehicleUIEvent
-    //    object CancelRegisterVehicleButtonClicked:RegisterVehicleUIEvent
+    object CancelRegisterVehicleButtonClicked:RegisterVehicleUIEvent
     data class SetBrand(val brand: String) : RegisterVehicleUIEvent
     data class SetModel(val model: String) : RegisterVehicleUIEvent
     data class SetYear(val year: Int) : RegisterVehicleUIEvent
     data class SetVehicleClass(val vehicleClass: String) : RegisterVehicleUIEvent
     data class SetLicensePlate(val licensePlate: String) : RegisterVehicleUIEvent
     data class SetImgLink(val imgLink: Int) : RegisterVehicleUIEvent
-    data class SetLatitude(val latitude: Float) : RegisterVehicleUIEvent
-    data class SetLongitude(val longitude: Float) : RegisterVehicleUIEvent
     data class SetPrice(val price: Double) : RegisterVehicleUIEvent
     object ResetVehicleUpdated: RegisterVehicleUIEvent
+
+    object OnAddressAutoCompleteClear : RegisterVehicleUIEvent
+    data class OnAddressChange(val address: String) : RegisterVehicleUIEvent
+    data class OnAddressSelected(val selectedPlaceItem: PlaceItem) : RegisterVehicleUIEvent
 }
