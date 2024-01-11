16c8047573594e8b7b85709d4b4869b8a5cdbffc
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-01-11 13:06:32 +0100

    implemented Location Google Place API autosuggestions

--- app/src/main/java/com/digitalarchitects/rmc_app/presentation/screens/registervehicle/RegisterVehicleScreen.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/presentation/screens/registervehicle/RegisterVehicleScreen.kt
@@ -4,16 +4,22 @@ import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -27,6 +33,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -35,10 +42,14 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.digitalarchitects.rmc_app.R
+import com.digitalarchitects.rmc_app.domain.model.AddressItem
 import com.digitalarchitects.rmc_app.domain.model.EngineType
+import com.digitalarchitects.rmc_app.domain.model.PlaceItem
 import com.digitalarchitects.rmc_app.presentation.RmcScreen
+import com.digitalarchitects.rmc_app.presentation.components.AutoCompleteUI
 import com.digitalarchitects.rmc_app.presentation.components.RmcAppBar
 import com.digitalarchitects.rmc_app.presentation.components.RmcFilledButton
+import com.digitalarchitects.rmc_app.presentation.components.RmcOutlinedButton
 import com.digitalarchitects.rmc_app.presentation.components.RmcSpacer
 import com.digitalarchitects.rmc_app.presentation.components.RmcSwitch
 import com.digitalarchitects.rmc_app.presentation.components.RmcTextField
@@ -52,14 +63,21 @@ fun RegisterVehicleScreen(
     val uiState by viewModel.uiState.collectAsState()
     val vehicleUpdated by viewModel.vehicleUpdated.collectAsState()
 
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    val address by viewModel.address.collectAsState()
+    val placesPredictions by viewModel.placePredictions.collectAsState()
+    val showProgressbar by viewModel.showProgressBar.collectAsState()
+
     LaunchedEffect(Unit) {
         viewModel.onEvent(RegisterVehicleUIEvent.FetchUserId)
     }
+
     Scaffold(
         topBar = {
             RmcAppBar(
                 title = R.string.screen_title_register_vehicle,
-                navigationIcon = Icons.Rounded.ArrowBack,
+                navigationIcon = Icons.AutoMirrored.Rounded.ArrowBack,
                 navigateUp = {
                     navigateToScreen(RmcScreen.MyVehicles.name)
                 },
@@ -69,7 +87,8 @@ fun RegisterVehicleScreen(
         Surface(
             modifier = Modifier
                 .padding(innerPadding)
-                .fillMaxSize(),
+                .fillMaxSize()
+                .imePadding(),
             color = MaterialTheme.colorScheme.surface
         ) {
             Column(
@@ -274,77 +293,205 @@ fun RegisterVehicleScreen(
 
                     RmcSpacer(8)
 
-
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_large))
-                    ) {
-                        RmcTextField(
-                            label = stringResource(id = R.string.latitude),
-//                            icon = Icons.Filled.Person,
-                            value = uiState.latitude.toString(),
-                            keyboardOptions = KeyboardOptions.Default.copy(
-                                keyboardType = KeyboardType.Number,
-                                imeAction = ImeAction.Next
-                            ),
-                            onValueChange = { input ->
-                                // Check if the input is a valid float before updating the state
-                                if (input.isEmpty() || input.toFloatOrNull() != null) {
-                                    viewModel.onEvent(RegisterVehicleUIEvent.SetLatitude(input.toFloatOrNull() ?: 0f))
-                                }
-                            },
-                            modifier = Modifier.weight(1f)
-                        )
-                    }
-
-                    RmcSpacer(8)
-
-
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_large))
-                    ) {
-                        RmcTextField(
-                            label = stringResource(id = R.string.longitude),
-//                            icon = Icons.Filled.Person,
-                            value = uiState.longitude.toString(),
-                            keyboardOptions = KeyboardOptions.Default.copy(
-                                keyboardType = KeyboardType.Number,
-                                imeAction = ImeAction.Done
-                            ),
-                            onValueChange = { input ->
-                                // Check if the input is a valid float before updating the state
-                                if (input.isEmpty() || input.toFloatOrNull() != null) {
-                                    viewModel.onEvent(RegisterVehicleUIEvent.SetLongitude(input.toFloatOrNull() ?: 0f))
-                                }
-                            },
-                            modifier = Modifier.weight(1f)
-                        )
-                    }
-
-                    RmcSpacer(32)
-
-                    RmcFilledButton(
-                        value = stringResource(id = R.string.register),
-                        onClick = {
-                            viewModel.onEvent(RegisterVehicleUIEvent.ConfirmRegisterVehicleButtonClicked)
+                    AddressEdit(
+                        addressItem = address,
+                        modifier = Modifier,
+                        addressPlaceItemPredictions = placesPredictions,
+                        onQueryChanged = { query ->
+                            viewModel.onEvent(RegisterVehicleUIEvent.OnAddressChange(query))
+                        },
+                        onClearClick = {
+                            viewModel.onEvent(RegisterVehicleUIEvent.OnAddressAutoCompleteClear)
+                        },
+                        onDoneClick = if (placesPredictions.isNotEmpty()) {
+                            {
+                                viewModel.onEvent(
+                                    RegisterVehicleUIEvent.OnAddressSelected(
+                                        placesPredictions.first()
+                                    )
+                                )
+                            }
+                        } else {
+                            { keyboardController?.hide() }
+                        },
+                        onItemClick = { placeItem ->
+                            viewModel.onEvent(RegisterVehicleUIEvent.OnAddressSelected(placeItem))
                         }
                     )
-                    val context = LocalContext.current
-                    val toastMessage = if (vehicleUpdated) {
-                        stringResource(R.string.vehicle_registered_successfully)
-                    } else {
-                        stringResource(R.string.unable_to_register_vehicle)
+                }
+
+                if (showProgressbar) {
+                    CircularProgressIndicator(
+                        modifier = Modifier
+                            .size(50.dp)
+                            .align(Alignment.CenterHorizontally)
+                    )
+                }
+
+                RmcSpacer(32)
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium))
+                ) {
+                    Column(Modifier.weight(1f)) {
+                        RmcFilledButton(
+                            value = stringResource(id = R.string.register),
+                            onClick = {
+                                viewModel.onEvent(RegisterVehicleUIEvent.ConfirmRegisterVehicleButtonClicked)
+                            }
+                        )
                     }
-                    if (vehicleUpdated) {
-                        Toast.makeText(context, toastMessage, Toast.LENGTH_SHORT).show()
-                        navigateToScreen(RmcScreen.MyVehicles.name)
-                        viewModel.onEvent(RegisterVehicleUIEvent.ResetVehicleUpdated)
+                    Column(Modifier.weight(1f)) {
+                        RmcOutlinedButton(
+                            value = stringResource(id = R.string.cancel),
+                            onClick = {
+                                viewModel.onEvent(RegisterVehicleUIEvent.CancelRegisterVehicleButtonClicked)
+                                navigateToScreen(RmcScreen.MyAccount.name)
+                            }
+                        )
                     }
+                }
+                val context = LocalContext.current
+                val toastMessage = if (vehicleUpdated) {
+                    stringResource(R.string.vehicle_registered_successfully)
+                } else {
+                    stringResource(R.string.unable_to_register_vehicle)
+                }
+                if (vehicleUpdated) {
+                    Toast.makeText(context, toastMessage, Toast.LENGTH_SHORT).show()
+                    navigateToScreen(RmcScreen.MyVehicles.name)
+                    viewModel.onEvent(RegisterVehicleUIEvent.ResetVehicleUpdated)
                 }
             }
         }
     }
+}
+
+@Composable
+fun AddressEdit(
+    addressItem: AddressItem,
+    modifier: Modifier,
+    addressPlaceItemPredictions: List<PlaceItem>,
+    onQueryChanged: (String) -> Unit,
+    onClearClick: () -> Unit,
+    onDoneClick: () -> Unit,
+    onItemClick: (PlaceItem) -> Unit
+) {
+
+    Column(
+        modifier = modifier.padding(top = 8.dp, bottom = 8.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        AutoCompleteUI(
+            modifier = Modifier.fillMaxWidth(),
+            query = addressItem.streetAddress,
+            queryLabel = stringResource(id = R.string.vehicle_location),
+            useOutlined = true,
+            onQueryChanged = onQueryChanged,
+            predictions = addressPlaceItemPredictions,
+            onClearClick = onClearClick,
+            onDoneActionClick = onDoneClick,
+            onItemClick = onItemClick
+        ) {
+            Text(text = it.address, style = MaterialTheme.typography.bodyLarge)
+        }
+
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Column(modifier = Modifier.padding(horizontal = 8.dp)) {
+
+            Row(modifier = modifier.height(IntrinsicSize.Min)) {
+
+                Column(modifier = Modifier.weight(1f)) {
+
+                    Text("City", fontWeight = FontWeight.Bold)
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    Text(
+                        addressItem.city,
+                        modifier = Modifier.weight(1.0f)
+                    )
+                }
+
+
+
+                Column(modifier = Modifier.weight(1f)) {
+
+                    Text("State", fontWeight = FontWeight.Bold)
+
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    Text(
+                        addressItem.state,
+                        modifier = Modifier.weight(1.0f)
+                    )
+                }
+
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(modifier = modifier.height(IntrinsicSize.Min)) {
+
+                Column(modifier = Modifier.weight(1f)) {
+                    Text("Postal Code", fontWeight = FontWeight.Bold)
+
+                    Text(
+                        addressItem.postalCode,
+                        modifier = Modifier.weight(1.0f)
+                    )
+                }
+
+
+                Column(modifier = Modifier.weight(1f)) {
+                    Text("Country", fontWeight = FontWeight.Bold)
+
+                    Text(
+                        addressItem.country,
+                        modifier = Modifier.weight(1.0f)
+                    )
+
+                }
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(modifier = modifier.height(IntrinsicSize.Min)) {
+
+
+                Column(modifier = Modifier.weight(1f)) {
+
+                    Text("Latitude", fontWeight = FontWeight.Bold)
+
+
+
+                    Text(
+                        addressItem.latitude.toString(),
+                        modifier = Modifier.weight(1.0f)
+                    )
+                }
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                Column(modifier = Modifier.weight(1f)) {
+
+                    Text("Longitude", fontWeight = FontWeight.Bold)
+
+                    Text(
+                        addressItem.longitude.toString(),
+                        modifier = Modifier.weight(1.0f)
+                    )
+                }
+
+            }
+
+        }
+    }
+
 }
