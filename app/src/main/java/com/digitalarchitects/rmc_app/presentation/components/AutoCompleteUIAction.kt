31ac3026ebcf2a55003215303dd8c58271dc4536
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-01-11 13:27:34 +0100

    Hoisted AutoCompleteUIAction into AddressEdit Composable for cleaner code

--- app/src/main/java/com/digitalarchitects/rmc_app/presentation/components/AutoCompleteUIAction.kt
+++
@@ -1,9 +0,0 @@
-package com.digitalarchitects.rmc_app.presentation.components
-
-sealed class AutoCompleteUIAction {
-    object OnDoneClick : AutoCompleteUIAction()
-    object OnClearClick : AutoCompleteUIAction()
-
-    data class OnQueryChange(val query: String) : AutoCompleteUIAction()
-    data class OnItemSelected<T>(val selectedItem: T) : AutoCompleteUIAction()
-}
