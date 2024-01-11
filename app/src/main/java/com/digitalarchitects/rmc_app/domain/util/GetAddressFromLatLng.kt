56eea3aa01f71984c95594ca8fe7685aeb211789
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-01-11 19:50:33 +0100

    moved fun to own file

--- app/src/main/java/com/digitalarchitects/rmc_app/domain/util/GetAddressFromLatLng.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/domain/util/GetAddressFromLatLng.kt
@@ -1,5 +1,6 @@
 package com.digitalarchitects.rmc_app.domain.util
 
+import android.content.Context
 import android.location.Address
 import android.location.Geocoder
 import android.os.Build
@@ -30,5 +31,37 @@ suspend fun Geocoder.getAddressFromLatLng(
     } catch (e: Exception) {
         Log.e("Geocoder", "getAddressFromLatLng: ${e.message}")
         null
+    }
+}
+
+suspend fun getAddressFromLatLng(
+    context: Context,
+    latitude: Double,
+    longitude: Double,
+    detailed: Boolean
+): String {
+    return try {
+        val geoCoder = Geocoder(context)
+        val address = geoCoder.getAddressFromLatLng(latitude, longitude)
+
+        if (detailed) {
+            return address?.getAddressLine(0) ?: ""
+        }
+
+        // Exclude country and postalCode from the address when detailed is false
+        val addressLine = address?.getAddressLine(0) ?: ""
+        val country = address?.countryName.orEmpty()
+        val postalCode = address?.postalCode.orEmpty()
+
+        // Remove the country and postalCode from the address
+        val formattedAddress = addressLine
+            .replace(", $country", "")
+            .replace(", $postalCode", "")
+
+        formattedAddress
+    } catch (e: Exception) {
+        Log.d("MyVehiclesViewModel", "getAddressFromLatLng: ${e.message}")
+        e.printStackTrace()
+        ""
     }
 }
