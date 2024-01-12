7610ed4ac940ec66d053862e513e92084620bb16
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-01-12 11:33:21 +0100

    added UIEvents for Location feature

--- app/src/main/java/com/digitalarchitects/rmc_app/presentation/screens/search/SearchUIEvent.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/presentation/screens/search/SearchUIEvent.kt
@@ -1,13 +1,19 @@
 package com.digitalarchitects.rmc_app.presentation.screens.search
 
-sealed class SearchUIEvent {
-    data class DateChanged(val date: String) : SearchUIEvent()
-    data class LocationChanged(val location: String) : SearchUIEvent()
-    data class PriceChanged(val price: Float) : SearchUIEvent()
-    data class DistanceChanged(val distance: Float) : SearchUIEvent()
-    data class EngineTypeIceChanged(val selected: Boolean) : SearchUIEvent()
-    data class EngineTypeCevChanged(val selected: Boolean) : SearchUIEvent()
-    data class EngineTypeFbevChanged(val selected: Boolean) : SearchUIEvent()
-    object ApplyFiltersButtonClicked : SearchUIEvent()
-    object ClearFiltersButtonClicked : SearchUIEvent()
+import com.digitalarchitects.rmc_app.domain.model.PlaceItem
+import kotlinx.datetime.LocalDate
+
+sealed interface SearchUIEvent {
+    object FetchFilterPreference : SearchUIEvent
+    data class DateChanged(val date: LocalDate) : SearchUIEvent
+    data class PriceChanged(val price: Float) : SearchUIEvent
+    data class DistanceChanged(val distance: Float) : SearchUIEvent
+    data class EngineTypeIceChanged(val selected: Boolean) : SearchUIEvent
+    data class EngineTypeBevChanged(val selected: Boolean) : SearchUIEvent
+    data class EngineTypeFcevChanged(val selected: Boolean) : SearchUIEvent
+    object ApplyFiltersButtonClicked : SearchUIEvent
+    object ClearFiltersButtonClicked : SearchUIEvent
+    object OnAddressAutoCompleteClear : SearchUIEvent
+    data class OnAddressChange(val address: String) : SearchUIEvent
+    data class OnAddressSelected(val selectedPlaceItem: PlaceItem) : SearchUIEvent
 }
