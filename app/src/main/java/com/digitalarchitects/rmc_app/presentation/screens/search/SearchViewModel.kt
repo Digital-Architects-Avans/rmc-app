3e9d471678869d11808ad0403b3ebda3186f514b
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-01-12 11:32:44 +0100

    added Location feature to SearchViewModel

--- app/src/main/java/com/digitalarchitects/rmc_app/presentation/screens/search/SearchViewModel.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/presentation/screens/search/SearchViewModel.kt
@@ -1,29 +1,112 @@
 package com.digitalarchitects.rmc_app.presentation.screens.search
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.digitalarchitects.rmc_app.data.di.IoDispatcher
+import com.digitalarchitects.rmc_app.domain.model.AddressItem
+import com.digitalarchitects.rmc_app.domain.model.PlaceItem
+import com.digitalarchitects.rmc_app.domain.repo.PlacesRepository
+import com.digitalarchitects.rmc_app.domain.repo.UserPreferencesRepository
+import com.digitalarchitects.rmc_app.domain.util.Result
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlinx.datetime.LocalDate
 import javax.inject.Inject
 
 @HiltViewModel
 class SearchViewModel @Inject constructor(
+    private val userPreferencesRepository: UserPreferencesRepository,
+    private val placesRepository: PlacesRepository,
+    @IoDispatcher private val dispatcher: CoroutineDispatcher
 ) : ViewModel() {
+
     private val _uiState = MutableStateFlow(SearchUIState())
     val uiState: StateFlow<SearchUIState> = _uiState.asStateFlow()
 
-    // TODO: Initialize search options based on current settings, passed in by the NavHost
-    init {
-        _uiState.value = _uiState.value.copy(
-            date = "Date from NavHost",
-            location = "Location from NavHost",
-            price = 0.0,
-            distance = 0,
-            engineTypeIce = true,
-            engineTypeBev = false,
-            engineTypeFcev = true
-        )
+    private val _address: MutableStateFlow<AddressItem> = MutableStateFlow(AddressItem())
+    val address = _address.asStateFlow()
+
+    private val _placePredictions: MutableStateFlow<List<PlaceItem>> =
+        MutableStateFlow(arrayListOf())
+    val placePredictions = _placePredictions.asStateFlow()
+
+    private fun getPlacePredictions(query: String) {
+        viewModelScope.launch(dispatcher) {
+
+            when (val placePredictionsResult = placesRepository.getPlacePredictions(query)) {
+                is Result.Success -> {
+                    val placePredictions = placePredictionsResult.data
+
+                    _placePredictions.value = placePredictions
+                }
+
+                is Result.Error -> {
+                    Log.e(
+                        "RegisterVehicleViewModel",
+                        "An error occurred when retrieving the predictions for $query",
+                        placePredictionsResult.exception
+                    )
+                }
+
+                else -> {}
+            }
+        }
+    }
+
+    private suspend fun setLocationPrediction(placeItem: PlaceItem) {
+        Log.d("RegisterVehicleViewModel", "placeItem: $placeItem")
+
+        viewModelScope.launch(dispatcher) {
+            when (val addressResult = placesRepository.getLocationFromPlace(placeItem.id!!)) {
+
+                is Result.Success -> {
+                    val addressFromPlace = addressResult.data
+                    if (addressFromPlace != null) {
+                        _address.value = addressFromPlace
+                        _uiState.update {
+                            it.copy(
+                                latitude = addressFromPlace.latitude.toFloat()
+                            )
+                        }
+                        _uiState.update {
+                            it.copy(
+                                longitude = addressFromPlace.longitude.toFloat()
+                            )
+                        }
+                    }
+                    clearPredictions()
+                }
+
+                is Result.Error -> {
+                    Log.e(
+                        "RegisterVehicleViewModel",
+                        "An error occurred when retrieving the address from Place  ${placeItem.id}",
+                        addressResult.exception
+                    )
+                }
+
+                else -> {}
+            }
+        }
+    }
+
+    private fun onLocationAutoCompleteClear() {
+        viewModelScope.launch {
+            _address.value = AddressItem()
+            clearPredictions()
+        }
+    }
+
+
+    private fun clearPredictions() {
+        _placePredictions.value = mutableListOf()
     }
 
     fun onEvent(event: SearchUIEvent) {
@@ -31,12 +114,6 @@ class SearchViewModel @Inject constructor(
             is SearchUIEvent.DateChanged -> {
                 _uiState.value = _uiState.value.copy(
                     date = event.date
-                )
-            }
-
-            is SearchUIEvent.LocationChanged -> {
-                _uiState.value = _uiState.value.copy(
-                    location = event.location
                 )
             }
 
@@ -48,7 +125,7 @@ class SearchViewModel @Inject constructor(
 
             is SearchUIEvent.DistanceChanged -> {
                 _uiState.value = _uiState.value.copy(
-                    distance = event.distance.toInt()
+                    distance = event.distance.toDouble()
                 )
             }
 
@@ -58,13 +135,13 @@ class SearchViewModel @Inject constructor(
                 )
             }
 
-            is SearchUIEvent.EngineTypeCevChanged -> {
+            is SearchUIEvent.EngineTypeBevChanged -> {
                 _uiState.value = _uiState.value.copy(
                     engineTypeBev = !event.selected
                 )
             }
 
-            is SearchUIEvent.EngineTypeFbevChanged -> {
+            is SearchUIEvent.EngineTypeFcevChanged -> {
                 _uiState.value = _uiState.value.copy(
                     engineTypeFcev = !event.selected
                 )
@@ -77,19 +154,95 @@ class SearchViewModel @Inject constructor(
             is SearchUIEvent.ApplyFiltersButtonClicked -> {
                 applyFilters()
             }
+
+            is SearchUIEvent.FetchFilterPreference -> {
+                viewModelScope.launch(dispatcher) {
+                    try {
+                        withContext(dispatcher) {
+                            val filterPreferences = userPreferencesRepository.getFilterPreference()
+                            Log.d("SearchViewModel", "date: ${filterPreferences.date}")
+
+                            // If user has set a date preference, convert it to LocalDate else null
+                            val dateAsLocalDate: LocalDate? = if (filterPreferences.date.isNullOrEmpty()) {
+                                LocalDate.parse(filterPreferences.date)
+                            } else {
+                                null
+                            }
+
+                            _uiState.value = _uiState.value.copy(
+                                date = dateAsLocalDate,
+                                latitude = filterPreferences.latitude,
+                                longitude = filterPreferences.longitude,
+                                price = filterPreferences.price,
+                                distance = filterPreferences.distance,
+                                engineTypeIce = filterPreferences.engineTypeICE,
+                                engineTypeBev = filterPreferences.engineTypeBEV,
+                                engineTypeFcev = filterPreferences.engineTypeFCEV
+                            )
+                        }
+                        Log.d("SearchViewModel", "Fetched preferences successfully")
+
+                    } catch (e: Exception) {
+                        Log.d("SearchViewModel", "Error fetching filter preference: $e")
+                    }
+                }
+            }
+
+
+            is SearchUIEvent.OnAddressAutoCompleteClear -> {
+                viewModelScope.launch {
+                    onLocationAutoCompleteClear()
+                }
+            }
+
+            is SearchUIEvent.OnAddressChange -> {
+                viewModelScope.launch {
+
+                    _address.update {
+                        it.copy(
+                            streetAddress = event.address
+                        )
+                    }
+                    getPlacePredictions(event.address)
+                }
+            }
+
+            is SearchUIEvent.OnAddressSelected -> {
+                viewModelScope.launch {
+                    setLocationPrediction(event.selectedPlaceItem)
+                }
+            }
         }
     }
 
     private fun applyFilters() {
-        TODO("Implement callback to NavHost with filters to Rent A Car view for search")
+        val state = _uiState.value
+        viewModelScope.launch(dispatcher) {
+            try {
+                userPreferencesRepository.saveFilterPreference(
+                    date = state.date.toString(),
+                    latitude = state.latitude,
+                    longitude = state.longitude,
+                    price = state.price,
+                    distance = state.distance,
+                    engineTypeICE = state.engineTypeIce,
+                    engineTypeBEV = state.engineTypeBev,
+                    engineTypeFCEV = state.engineTypeFcev
+                )
+                Log.d("SearchViewModel", "Saved preferences successfully, $state")
+            } catch (e: Exception) {
+                Log.d("SearchViewModel", "Error applying filter preference: $e")
+            }
+        }
     }
 
     private fun clearFilters() {
         _uiState.value = _uiState.value.copy(
-            date = "",
-            location = "",
+            date = null,
+            latitude = 0.0F,
+            longitude = 0.0F,
             price = 0.0,
-            distance = 0,
+            distance = 0.0,
             engineTypeIce = true,
             engineTypeBev = true,
             engineTypeFcev = true
