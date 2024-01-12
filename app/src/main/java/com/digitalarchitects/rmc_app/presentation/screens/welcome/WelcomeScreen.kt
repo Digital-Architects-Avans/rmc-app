a5395be8156fdfdd74c3daa96dbd9c99485030fe
Paul van der Linden <vderlinden@gmail.com>
2024-01-12 14:02:20 +0100

    Updated material to material3

--- app/src/main/java/com/digitalarchitects/rmc_app/presentation/screens/welcome/WelcomeScreen.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/presentation/screens/welcome/WelcomeScreen.kt
@@ -11,8 +11,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Surface
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
