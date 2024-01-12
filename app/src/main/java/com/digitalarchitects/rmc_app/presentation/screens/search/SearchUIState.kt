2ba5e75605a7400df22a3cc826ccd6af3f5efdf4
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-01-12 11:33:01 +0100

    added lat long and LocalDate to UiState

--- app/src/main/java/com/digitalarchitects/rmc_app/presentation/screens/search/SearchUIState.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/presentation/screens/search/SearchUIState.kt
@@ -1,12 +1,15 @@
 package com.digitalarchitects.rmc_app.presentation.screens.search
 
+import kotlinx.datetime.LocalDate
+
 // TODO: Hoist all search settings to Rent A Car screen to show results
 
 data class SearchUIState(
-    val date: String? = "Date!", // TODO: Make LocalDate, maybe with LocalDate.now()
-    val location: String? = "Location!",
-    val price: Double? = 55.0,
-    val distance: Int? = 53,
+    var date: LocalDate? = null,
+    val latitude: Float = 0.0F,
+    val longitude: Float = 0.0F,
+    val price: Double = 0.0,
+    val distance: Double = 0.0,
     val engineTypeIce: Boolean = true,
     val engineTypeBev: Boolean = true,
     val engineTypeFcev: Boolean = true
