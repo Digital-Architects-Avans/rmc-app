67bf64e1331658ee8b0441f75f900c65bea773b3
Brian174tech <114503613+Brian174tech@users.noreply.github.com>
2024-01-13 16:03:27 +0100

    Made login en register uievent an interface

--- app/src/main/java/com/digitalarchitects/rmc_app/presentation/screens/register/RegisterUIEvent.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/presentation/screens/register/RegisterUIEvent.kt
@@ -1,17 +1,17 @@
 package com.digitalarchitects.rmc_app.presentation.screens.register
 
 
-sealed class RegisterUIEvent{
+sealed interface RegisterUIEvent{
 
-    data class FirstNameChanged(val firstName: String) : RegisterUIEvent()
-    data class LastNameChanged(val lastName: String) : RegisterUIEvent()
-    data class EmailChanged(val email: String): RegisterUIEvent()
-    data class TelephoneChanged(val telephone: String) : RegisterUIEvent()
-    data class PasswordChanged(val password: String) : RegisterUIEvent()
-    data class AddressChanged(val address: String) : RegisterUIEvent()
-    data class PostalCodeChanged(val postalCode: String) : RegisterUIEvent()
-    data class BuildingNumberChanged(val buildingNumber: String) : RegisterUIEvent()
-    data class CityChanged(val city: String) : RegisterUIEvent()
-    data class PrivacyPolicyCheckBoxClicked(val status:Boolean) : RegisterUIEvent()
-    object RegisterButtonClicked : RegisterUIEvent()
+    data class FirstNameChanged(val firstName: String) : RegisterUIEvent
+    data class LastNameChanged(val lastName: String) : RegisterUIEvent
+    data class EmailChanged(val email: String): RegisterUIEvent
+    data class TelephoneChanged(val telephone: String) : RegisterUIEvent
+    data class PasswordChanged(val password: String) : RegisterUIEvent
+    data class AddressChanged(val address: String) : RegisterUIEvent
+    data class PostalCodeChanged(val postalCode: String) : RegisterUIEvent
+    data class BuildingNumberChanged(val buildingNumber: String) : RegisterUIEvent
+    data class CityChanged(val city: String) : RegisterUIEvent
+    data class PrivacyPolicyCheckBoxClicked(val status:Boolean) : RegisterUIEvent
+    object RegisterButtonClicked : RegisterUIEvent
 }
