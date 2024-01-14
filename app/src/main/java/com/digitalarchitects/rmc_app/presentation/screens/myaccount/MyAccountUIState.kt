d565064cb415cd541618ededdbd2a577c3c88d1a
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-01-15 00:19:32 +0100

    added imageResourceId

--- app/src/main/java/com/digitalarchitects/rmc_app/presentation/screens/myaccount/MyAccountUIState.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/presentation/screens/myaccount/MyAccountUIState.kt
@@ -1,10 +1,9 @@
 package com.digitalarchitects.rmc_app.presentation.screens.myaccount
 
-import com.digitalarchitects.rmc_app.R
 import com.digitalarchitects.rmc_app.domain.model.User
 
 data class MyAccountUIState(
     var users: List<User> = emptyList(),
     var currentUser: User? = null,
-    var imageResourceId: Int = R.drawable.usericon
+    var profileImgSrc: String? = null,
 )
