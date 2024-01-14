c1cb3c44ce0f844832fd6d450f551aa8331d1552
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-01-15 00:14:19 +0100

    added ProfileImage Composable to implement editing profile image feature

--- app/src/main/java/com/digitalarchitects/rmc_app/presentation/screens/editmyaccount/EditMyAccountScreen.kt
+++ app/src/main/java/com/digitalarchitects/rmc_app/presentation/screens/editmyaccount/EditMyAccountScreen.kt
@@ -1,16 +1,28 @@
 package com.digitalarchitects.rmc_app.presentation.screens.editmyaccount
 
+import android.content.Context
+import android.net.Uri
+import android.os.Environment
+import android.util.Log
 import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.ArrowBack
 import androidx.compose.material.icons.filled.Call
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Home
@@ -18,20 +30,33 @@ import androidx.compose.material.icons.filled.LocationCity
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Numbers
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material3.BasicAlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogProperties
+import androidx.core.content.FileProvider
+import coil.compose.rememberAsyncImagePainter
 import com.digitalarchitects.rmc_app.R
 import com.digitalarchitects.rmc_app.presentation.RmcScreen
 import com.digitalarchitects.rmc_app.presentation.components.RmcAppBar
@@ -39,6 +64,10 @@ import com.digitalarchitects.rmc_app.presentation.components.RmcFilledButton
 import com.digitalarchitects.rmc_app.presentation.components.RmcOutlinedButton
 import com.digitalarchitects.rmc_app.presentation.components.RmcSpacer
 import com.digitalarchitects.rmc_app.presentation.components.RmcTextField
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 
 @Composable
@@ -58,7 +87,7 @@ fun EditMyAccountScreen(
         topBar = {
             RmcAppBar(
                 title = R.string.screen_title_edit_account,
-                navigationIcon = Icons.Rounded.ArrowBack,
+                navigationIcon = Icons.AutoMirrored.Rounded.ArrowBack,
                 navigateUp = {
                     navigateToScreen(RmcScreen.MyAccount.name)
                 }
@@ -80,6 +109,24 @@ fun EditMyAccountScreen(
                     )
                     .verticalScroll(rememberScrollState())
             ) {
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    ProfileImage(
+                        profileImageSrc = if (!uiState.imgUpdated) {
+                            uiState.profileImageSrc
+                        } else {
+                            uiState.imageUri.toString()
+                        },
+                        onImageSelected = { selectedUri ->
+                            viewModel.onEvent(EditMyAccountUIEvent.SetImageUri(selectedUri))
+                            Log.d("EditMyAccountScreen", "selectedUri: $selectedUri")
+                        }
+                    )
+                }
                 Row(
                     modifier = Modifier,
                     horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium))
@@ -259,4 +306,131 @@ fun EditMyAccountScreen(
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProfileImage(
+    profileImageSrc: String?,
+    onImageSelected: (Uri) -> Unit
+) {
+    var imageUri by rememberSaveable { mutableStateOf<Uri?>(null) }
+    var showDialog by rememberSaveable { mutableStateOf(false) }
+
+    val painter = rememberAsyncImagePainter(
+        profileImageSrc ?: Uri.parse(R.drawable.usericon.toString())
+    )
+
+    val context = LocalContext.current
+    val galleryLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        uri?.let {
+            imageUri = it
+            onImageSelected(it)
+        }
+    }
+
+    // Permission is granted, Initialize the cameraLauncher
+    val cameraLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.TakePicture()
+    ) { isSuccess: Boolean ->
+        if (isSuccess) {
+            // Callback to handle the captured image
+            imageUri?.let { onImageSelected(it) }
+            showDialog = false
+        }
+    }
+
+    val cameraPermissionLauncher =
+        rememberLauncherForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                // Get a new image URI
+                val newImageUri = createImageUri(context)
+                imageUri = newImageUri
+
+                // Launch the camera
+                cameraLauncher.launch(newImageUri)
+                showDialog = false
+            } else {
+                // Permission is denied, close the dialog
+                showDialog = false
+            }
+        }
+
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Card(
+            shape = CircleShape,
+            modifier = Modifier
+                .padding(8.dp)
+                .size(100.dp)
+        ) {
+            Image(
+                painter = painter,
+                contentDescription = stringResource(id = R.string.profile_picture),
+                modifier = Modifier
+                    .wrapContentSize()
+                    .clickable {
+                        // Open the dialog asking for image source
+                        showDialog = true
+                    },
+                contentScale = ContentScale.Crop
+            )
+        }
+    }
+
+    if (showDialog) {
+        BasicAlertDialog(
+            onDismissRequest = { showDialog = false },
+            properties = DialogProperties(
+                dismissOnBackPress = true,
+                dismissOnClickOutside = true
+            ),
+            content = {
+                Column {
+                    Text(
+                        text = "Select Image Source",
+                        style = MaterialTheme.typography.bodySmall,
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .fillMaxWidth()
+                    )
+                    TextButton(
+                        onClick = {
+                            galleryLauncher.launch("image/*")
+                            showDialog = false
+                        }
+                    ) {
+                        Text(text = "Gallery")
+                    }
+                    TextButton(
+                        onClick = {
+                            // Before opening the camera, check for permission
+                            cameraPermissionLauncher.launch(android.Manifest.permission.CAMERA)
+                            showDialog = false
+                        }
+                    ) {
+                        Text(text = "Camera")
+                    }
+                }
+            }
+        )
+    }
+}
+
+private fun createImageUri(context: Context): Uri {
+    val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+    val storageDir: File? = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+    return FileProvider.getUriForFile(
+        context,
+        "${context.packageName}.provider",
+        File.createTempFile("JPEG_${timeStamp}_", ".jpg", storageDir)
+    )
 }
