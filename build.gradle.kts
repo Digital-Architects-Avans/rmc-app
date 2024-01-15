b1c6a43a780c788a8eba01f7c0d58bcaceb0ec47
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-01-15 18:34:25 +0100

    added firebase and google dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
 }
 
 buildscript {
